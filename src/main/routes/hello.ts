ab7910eaeac0be1ddefb2286bc768c0c7bee4865
Kazys Sketrys <kazys.sketrys@triad.co.uk>
2017-09-14 10:33:38 +0100

    Fixed failing test

--- src/main/routes/hello.ts
+++ src/main/routes/hello.ts
@@ -1,5 +1,8 @@
+import * as config from 'config'
 import * as express from 'express'
 import rq from 'client/request'
+
+const barApiUrl = config.get<string>('bar.url')
 
 // export the express.Router object with defined routes
 export default express.Router()
@@ -8,7 +11,7 @@ export default express.Router()
   .get('/hello', (req: express.Request, res: express.Response) => {
 
     // make request to this URL, and return "promise" / "error"
-    rq.get('http://localhost:8080/hello')
+    rq.get(`${barApiUrl}/hello`)
       .then(response => {
         res.status(200).render('bar/views/hello', {
           title: 'BARS Project',
