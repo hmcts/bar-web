b0278088addf968925eff95797d1983a1e640dc8
Sho-Silva Carter-Daniel <sho-silvacarter-daniel@Sho-Silvas-MacBook-Pro.local>
2017-10-03 10:41:20 +0100

    All changes made.

--- src/main/public/js/services.js
+++ src/main/public/js/services.js
@@ -5,8 +5,15 @@
 
   setTimeout(function () {
 
+    // Services Service
     var Service = {
-      getServices: function () {
+      getSubServiceByServiceId: function () {
+        return $.ajax({
+          method: 'GET',
+          url: '/posts/record/services'
+        });
+      },
+      getServicesAndSubServices: function () {
         return $.ajax({
           method: 'GET',
           url: '/posts/record/services'
@@ -14,13 +21,15 @@
       }
     };
 
+    // Service Application
     var ServicesApplication = {
       servicesDropDown: $('select[name=services]'),
       subServicesDropDown: $('select[name=sub-services]'),
       inputControl: $('[class=form-control]'),
+      servicesAndSubServices: [],
 
       init: function () {
-        console.log('Application has started');
+        this.loadServicesAndSubServices();
         this.setEvents();
       },
 
@@ -28,30 +37,43 @@
         this.servicesDropDown.on('change', this.onServiceChange.bind(this));
       },
 
+      // START: event triggered methods goes here
       onServiceChange: function (ev) {
         var serviceId = parseInt($(ev.currentTarget).val());
-        Service.getServices().then(function (response) {
-          var self = this;
-          response.forEach(function (service) {
-            if (service.id === serviceId) {
-              if (service.subServices.length) {
-                self.subServicesDropDown.empty();
-                self.onPopulateSubServices(service.subServices);
-              }
-            }
-          });
+        this.getSubServicesById(serviceId);
+      },
+      // END: event triggered methods goes here
+
+      loadServicesAndSubServices: function () {
+        Service.getServicesAndSubServices().then(function (response) {
+          this.servicesAndSubServices = response;
         }.bind(this));
       },
 
-      onPopulateSubServices: function (subServices) {
-        subServices.forEach(function (option) {
-          this.subServicesDropDown.append($('<option value="'+ option.id +'">' + option.name + '</option>'));
+      getSubServicesById: function (serviceId) {
+        serviceId--; // since an array starts from 0, decrease by one
+        if (this.servicesAndSubServices.length < 1) {
+          Service.getSubServiceByServiceId(serviceId).then(function (response) {
+            this.populateSubServices(response[serviceId].subServices);
+          }.bind(this));
+          return;
+        }
+
+        this.populateSubServices(this.servicesAndSubServices[serviceId].subServices);
+      },
+
+      populateSubServices: function (subServices) {
+        this.subServicesDropDown.empty();
+        subServices.forEach(function (subService) {
+          this.subServicesDropDown.append( $('<option value="'+subService.id+'">'+subService.name+'</option>') );
         }.bind(this))
       }
     };
 
+    // instantiate the app
     ServicesApplication.init();
-  }, 1000);
+
+  }, 1500);
 
 
 })(jQuery);
--- src/main/routes/paymenttypes.ts
+++ src/main/routes/paymenttypes.ts
@@ -4,12 +4,13 @@ import * as PaymentService from 'common/services/payments'
 export default express.Router()
 
   .get('/posts/record', (req: express.Request, res: express.Response) => {
-    const getServicesWithSubServices = PaymentService.getServicesWithSubservices()
-    const getPaymentTypes = PaymentService.getPaymentTypes()
-
-    getServicesWithSubServices.then(services => {
-      getPaymentTypes.then(paymentTypes => {
-        res.status(200).render('posts/index', {services, paymentTypes})
+    PaymentService.getServicesWithSubservices().then(services => {
+      let preSelectedSubServices = []
+      if (services.length > 0 && typeof services[0].subServices !== 'undefined') {
+        preSelectedSubServices = services[0].subServices
+      }
+      PaymentService.getPaymentTypes().then(paymentTypes => {
+        res.status(200).render('posts/index', {services, paymentTypes, preSelectedSubServices})
       })
     }).catch(error => {
       res.render('posts/error', {error})
@@ -17,10 +18,7 @@ export default express.Router()
   })
 
   .get('/posts/record/services', (req: express.Request, res: express.Response) => {
-    const getServicesWithSubServices = PaymentService.getServicesWithSubservices()
-
-    getServicesWithSubServices
-      .then(services => {
-        res.status(200).json(services)
-      })
+    PaymentService.getServicesWithSubservices().then(services => {
+      res.status(200).json(services)
+    })
   })
