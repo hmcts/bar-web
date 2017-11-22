0dc53e4a04bc2f78e7c68a368f182060add1d83d
Sho-Silva Carter-Daniel <sho-silvacarter-daniel@Sho-Silvas-MacBook-Pro.local>
2017-11-22 17:31:30 +0000

    Change from Axios to Request.

--- express/mvc/controllers/PaymentsController.js
+++ express/mvc/controllers/PaymentsController.js
@@ -4,7 +4,7 @@ const PaymentService = require('../../services').PaymentService;
 // export payment types controller
 exports.GET_index = (req, res) => {
   PaymentService.getPaymentTypes()
-    .then(response => { res.json(response.data); })
+    .then(response => res.json(response.body))
     .catch(error => res.json({ error: error.message }));
 };
 
@@ -13,6 +13,6 @@ exports.POST_index = (req, res) => {
   const paymentType = req.params.type;
 
   PaymentService.sendPaymentDetails(data, paymentType)
-    .then(status => { res.json(status.data) })
+    .then(response => { console.log( response.body ); res.json(response.body) })
     .catch(error => { res.json({ error: error.message }); });
 };
--- express/services/PaymentService.js
+++ express/services/PaymentService.js
@@ -1,13 +1,15 @@
 const axios = require('axios');
 const config = require('config');
+const request = require('client-request/promise');
 const barUrl = config.get('bar.url');
 
 exports.getPaymentTypes = () => {
   console.log( `Trying to reach: ${barUrl}/payment-types` );
 
-  return axios({
-    method: 'get',
-    url: `${barUrl}/payment-types`
+  return request({
+    uri: `${barUrl}/payment-types`,
+    method: "GET",
+    json: true
   })
 };
 
@@ -18,12 +20,15 @@ exports.sendPaymentDetails = (data, type) => {
   data.payment_type = type;
 
   // implement this temporarily now
-  data.account_number = '00000000';
-  data.sort_code = '000000';
+  // data.account_number = '00000000';
+  // data.sort_code = '000000';
 
-  return axios({
-    method: 'post',
-    url: `${barUrl}/${type}`,
-    data: data
+  console.log( data );
+
+  return request({
+    uri: `${barUrl}/${type}`,
+    method: "POST",
+    body: data,
+    json: true
   })
 };
