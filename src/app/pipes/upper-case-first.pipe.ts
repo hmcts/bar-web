e913036711ea487e5e52b6710780185c8dcfe0b8
Sho-Silva Carter-Daniel <sho-silvacarter-daniel@Sho-Silvas-MacBook-Pro.local>
2017-11-29 17:24:37 +0000

    Payment Instruction page correction done (removed trailing whitespace).

--- src/app/pipes/upper-case-first.pipe.ts
+++ src/app/pipes/upper-case-first.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'upperCaseFirst'
 })
 export class UpperCaseFirstPipe implements PipeTransform {
-  
+
   transform(value: any, args?: any): any {
     if (value.length < 1) {
       return null;
