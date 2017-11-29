f82a1a9143a0ed7d9704fe13c810f8b1c8f7754c
Sho-Silva Carter-Daniel <sho-silvacarter-daniel@Sho-Silvas-MacBook-Pro.local>
2017-11-29 17:21:38 +0000

    Payment Instruction page correction done (with payment logs).

--- src/app/pipes/upper-case-first.pipe.ts
+++ src/app/pipes/upper-case-first.pipe.ts
@@ -4,11 +4,11 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'upperCaseFirst'
 })
 export class UpperCaseFirstPipe implements PipeTransform {
-
+  
   transform(value: any, args?: any): any {
-  	if (value.length < 1) {
-  		return null;
-  	}
+    if (value.length < 1) {
+      return null;
+    }
     const firstLetter = value.charAt(0).toUpperCase();
     return firstLetter + value.substring(1, value.length);
   }
