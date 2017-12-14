c823f1a4e73d2b3c7b4f59a97bc75118d5bfae0d
aberegszaszi <attila.beregszaszi@hmcts.net>
2017-12-14 17:57:30 +0000

    Boilerplate for in-repo bar-web functional tests

--- e2e/codecept.conf.js
+++ e2e/codecept.conf.js
@@ -1,31 +1,27 @@
 const ProxySettings = require('./proxy-settings');
+const config = require('config');
 
-const timeout = process.env.E2E_TIMEOUT || 10000;
-const waitForTimeout = process.env.E2E_WAITFOR_TIMEOUT || 15000;
+const { timeout, webdriverio } = config;
+const { host, port, browser, url, waitForTimeout } = webdriverio;
 
-const config = {
-  name: 'bar-web-acceptance-tests',
-  tests: './test/end-to-end/tests/*_test.js',
-  timeout,
-  output: './output',
-  helpers: {
-    WebDriverIO: {
-      host: process.env.WEB_DRIVER_HOST || 'localhost',
-      port: process.env.WEB_DRIVER_PORT || '4444',
-      browser: process.env.BROWSER || 'chrome',
-      url: process.env.URL || 'https://localhost:3415',
-      waitForTimeout,
-      desiredCapabilities: {
-        proxy: new ProxySettings()
+module.exports = {
+  config: {
+    tests: './paths/**.js',
+    timeout,
+    output: './output',
+    name: 'bar-web-e2e-tests',
+    helpers: {
+      WebDriverIO: {
+        host,
+        port,
+        browser,
+        waitForTimeout,
+        url,
+        desiredCapabilities: { proxy: new ProxySettings() }
       }
-    }
-  },
-  include: {
-    I: './test/end-to-end/pages/steps_file.js',
-    Idam: './test/end-to-end/pages/idam_backdoor.js'
-  },
-  bootstrap: false,
-  mocha: {}
+    },
+    include: { I: './pages/steps_file.js' },
+    bootstrap: false,
+    mocha: {}
+  }
 };
-
-module.exports = { config };
--- e2e/proxy-settings.js
+++ e2e/proxy-settings.js
@@ -3,10 +3,10 @@ const url = require('url');
 module.exports = class ProxySettings {
   constructor() {
     if (process.env.HTTP_PROXY) {
-      this.httpProxy = url.parse(process.env.HTTP_PROXY).host
+      this.httpProxy = url.parse(process.env.HTTP_PROXY).host;
     }
     if (process.env.HTTPS_PROXY) {
-      this.sslProxy = url.parse(process.env.HTTPS_PROXY).host
+      this.sslProxy = url.parse(process.env.HTTPS_PROXY).host;
     }
     if (process.env.NO_PROXY) {
       this.noProxy = process.env.NO_PROXY;
