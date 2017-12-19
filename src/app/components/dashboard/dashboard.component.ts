eb6c5b15a5050132e06576d11de86ba9c72d893b
Sho Carter-Daniel <shosilvadaniel@gmail.com>
2017-12-19 11:12:50 +0000

    Updated NodeEngine

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -30,13 +30,13 @@ export class DashboardComponent implements OnInit {
     private route: ActivatedRoute
   ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     if (!this.userService.getUser()) {
       // Return early by redirecting to login if not authenticated
       return this.router.navigateByUrl('/');
     }
 
-    this.loadPaymentTypes();
+    await this.loadPaymentTypes();
 
     this.route.params.subscribe(params => {
       if (typeof params.id !== 'undefined') {
@@ -66,18 +66,13 @@ export class DashboardComponent implements OnInit {
     }
   }
 
-  onInputPropertyChange($ev): void {
+  onInputPropertyChange(): void {
     // check if all the fields are empty or not
     if (this.hasPopulatedField()) {
       this.filledContent = true;
     } else {
       this.filledContent = false;
     }
-  }
-
-  onChangePaymentType(paymentType): void {
-    // revert back to initial date except amount and payee name
-    this.resetData();
   }
 
   onToggleShowModal(): void {
