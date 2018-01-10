858788d82b1064210db66c9a51788f3d7d2e8a2d
shosilva <ShoSilvaDaniel@hmcts.net>
2018-01-10 13:31:39 +0000

    Added to support testing. (#119)

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -9,15 +9,28 @@ export class UserService {
   constructor() { }
 
   getUser() {
-    return this.user;
+    if (localStorage.getItem('user') === null) {
+      return false;
+    }
+    return JSON.parse(localStorage.getItem('user'));
   }
 
   authenticate({ email, passw }): boolean {
     if (email === 'chris.spencer@hmcts.net' && passw === 'password') {
-      this.user = { fullName: 'Chris Spencer', email: email, courtId: 'BR01', role: 'clerk' };
+      this.storeUser({
+        fullName: 'Chris Spencer',
+        email: email,
+        courtId: 'BR01',
+        role: 'clerk'
+      });
       return true;
     } else if (email === 'fee.clerk@hmcts.net' && passw === 'password') {
-      this.user = { fullName: 'Fee Clerk', email: email, courtId: 'BR02', role: 'feeclerk' };
+      this.storeUser({
+        fullName: 'Fee Clerk',
+        email: email,
+        courtId: 'BR02',
+        role: 'feeclerk'
+      });
       return true;
     }
     return false;
@@ -27,8 +40,13 @@ export class UserService {
     this.user.email = '';
   }
 
+  storeUser(user: any) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
   logOut() {
     this.user = false;
+    localStorage.removeItem('user');
   }
 
 }
