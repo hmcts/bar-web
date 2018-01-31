7e52a6ccc659680940e2f606eb6281232bba6cc2
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-01-31 17:36:20 +0000

    Latest changes. Will finish in the morning.

--- src/app/models/feedetail.model.ts
+++ src/app/models/feedetail.model.ts
@@ -1,7 +1,12 @@
 export class FeeDetailModel {
+  amount: number;
+  case_fee_id: number;
   case_reference_id: number;
   fee_code: any;
-  amount: number;
   fee_description: string;
   fee_version: string;
+  refund_amount: number;
+  remission_amount: number;
+  remission_authorisation: string;
+  remission_benefiter: string;
 }
