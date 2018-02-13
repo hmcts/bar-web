dc1487ded806b76e76c3051af49c7a9d2e99571d
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-02-13 18:07:08 +0000

    Refactor of code.

--- src/app/models/feedetail.model.ts
+++ src/app/models/feedetail.model.ts
@@ -1,3 +1,5 @@
+import { FeeSearchModel } from './feesearch.model';
+
 export class FeeDetailModel {
   amount: number;
   case_fee_id: number;
@@ -10,4 +12,26 @@ export class FeeDetailModel {
   remission_amount: number;
   remission_authorisation: string;
   remission_benefiter: string;
+
+  getRemissionAmount() {
+    if (!this.hasOwnProperty('remission_amount') || this.remission_amount === null) {
+      return '-';
+    }
+
+    return `${(this.remission_amount / 100).toFixed(2)}`;
+  }
+
+  getAmount() {
+    return `${(this.amount / 100).toFixed(2)}`;
+  }
+
+  // @TODO: Create currentCase model
+  assign(feeCodeModel: FeeSearchModel, currentCase: any) {
+    this.amount = feeCodeModel.getAmount();
+    this.case_reference = currentCase.id;
+    this.case_reference_id = currentCase.id;
+    this.fee_code = feeCodeModel.code;
+    this.fee_description = feeCodeModel.current_version.description;
+    this.fee_version = feeCodeModel.current_version.version;
+  }
 }
