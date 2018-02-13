87936c62430af598761ddc5d55d1f2779efa0556
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-02-13 15:22:05 +0000

    Fixed 143

--- src/app/components/feelogedit/feelogedit.component.ts
+++ src/app/components/feelogedit/feelogedit.component.ts
@@ -11,6 +11,8 @@ import { PaymentstateService } from '../../state/paymentstate.service';
 import { CaseReference } from '../../models/case-reference';
 import { PaymentInstructionActionModel } from '../../models/payment-instruction-action.model';
 import { FeeDetailModel } from '../../models/feedetail.model';
+import { PaymentAction } from '../../models/paymentaction.model';
+import { PaymentStatus } from '../../models/paymentstatus.model';
 
 @Component({
   selector: 'app-feelogedit',
@@ -67,14 +69,22 @@ export class FeelogeditComponent implements OnInit {
     });
   }
 
-  async loadFeeById(feeId) {
-    try {
-      const request = await this.paymentLogService.getPaymentById(feeId);
-      if (request.success === true) {
-        this.model = request.data;
-      }
-    } catch (e) {
-      console.log( e );
+  async addFeeToCase() {
+    if (this.addRemissionOn && this.feeDetail.remission_amount > this.feeDetail.amount) {
+      // remission amount should never be more than amount
+      return;
+    }
+
+    if (this.feeDetail.remission_amount > 0) {
+      this.feeDetail.remission_amount = (this.feeDetail.remission_amount * 100);
+    }
+
+    this.feeDetail.amount = (this.feeDetail.amount * 100);
+
+    const [err, data] = await UtilService.toAsync(this.feeLogService.addFeeToCase(this.loadedId, this.feeDetail));
+    if (!err) {
+      this.toggleFeeDetailsModal();
+      this.loadFeeById(this.model.id);
     }
   }
 
@@ -102,51 +112,15 @@ export class FeelogeditComponent implements OnInit {
     this.paymentState.setCurrentOpenedFeeTab(this.openedTab);
   }
 
-  goBack() { this.location.back(); }
-
-  toggleAddRemissionBlock() { this.addRemissionOn = !this.addRemissionOn; }
-
-  toggleCaseModalWindow() { this.modalOn = !this.modalOn; }
-
-  toggleReturnModal() { this.returnModalOn = !this.returnModalOn; }
-
-  toggleSuspenseModal() { this.suspenseModalOn = !this.suspenseModalOn; }
-
-
-  returnPaymentToPostClerk() {
-    this.model.status = 'D';
-    this.toggleReturnModal();
-    return this.router.navigateByUrl('/feelog');
-  }
-
-  toggleFeeDetailsModal(paymentInstructionCase?) {
-    this.currentCaseView = paymentInstructionCase;
-
-    if (this.feeDetailsModal) {
-      this.feeCodes = [];
-      this.searchFeeModel = '';
-      this.feeDetail.amount = null;
-
-      this.feeDetail.fee_code = '';
-      this.feeDetail.fee_description = '';
-      this.feeDetail.fee_version = '';
-
-      this.feeDetail.remission_amount = null;
-      this.feeDetail.remission_benefiter = '';
-      this.feeDetail.remission_authorisation = '';
-      this.addRemissionOn = false;
+  async loadFeeById(feeId) {
+    try {
+      const request = await this.paymentLogService.getPaymentById(feeId);
+      if (request.success === true) {
+        this.model = request.data;
+      }
+    } catch (e) {
+      console.log( e );
     }
-
-    this.feeDetailsModal = !this.feeDetailsModal;
-  }
-
-  selectFee(feeCodeModel) {
-    this.feeDetail.amount = this.getFeeAmount(feeCodeModel);
-    this.feeDetail.case_reference = this.currentCaseView.id;
-    this.feeDetail.case_reference_id = this.currentCaseView.id; // why this is here? i don't know (if we already have case_reference)
-    this.feeDetail.fee_code = feeCodeModel.code;
-    this.feeDetail.fee_description = feeCodeModel.current_version.description;
-    this.feeDetail.fee_version = feeCodeModel.current_version.version;
   }
 
   async loadFeeCodesAndDescriptions() {
@@ -155,54 +129,6 @@ export class FeelogeditComponent implements OnInit {
       if (data.found) {
         this.feeCodes = data.fees;
       }
-    }
-  }
-
-  onKeyUpFeeCodesAndDescriptions($ev) {
-    if ($ev.which === 13) { this.loadFeeCodesAndDescriptions(); }
-  }
-
-  async addFeeToCase() {
-    if (this.feeDetail.remission_amount < this.feeDetail.amount) {
-      this.feeDetail.amount = (this.feeDetail.amount * 100);
-
-      if (this.feeDetail.remission_amount > 0) {
-        this.feeDetail.remission_amount = (this.feeDetail.remission_amount * 100);
-      }
-
-      const [err, data] = await UtilService.toAsync(this.feeLogService.addFeeToCase(this.loadedId, this.feeDetail));
-      if (!err) {
-        this.toggleFeeDetailsModal();
-        this.loadFeeById(this.model.id);
-      }
-    }
-  }
-
-  async onSuspenseFormSubmit($event: Event) {
-    $event.preventDefault();
-
-    if (this.paymentInstructionActionModel.hasOwnProperty('reason')) {
-      const [err, data] = await UtilService
-        .toAsync(this.feeLogService.sendPaymentInstructionAction(this.model, this.paymentInstructionActionModel));
-
-      if (!err && data.success === true) {
-        this.paymentInstructionActionModel = new PaymentInstructionActionModel();
-        this.suspenseModalOn = !this.suspenseModalOn;
-        this.router.navigateByUrl('/feelog');
-      }
-    }
-  }
-
-
-  async onProcessPaymentSubmission() {
-    this.paymentInstructionActionModel.action = 'process';
-
-    const [err, data] = await UtilService
-      .toAsync(this.feeLogService.sendPaymentInstructionAction(this.model, this.paymentInstructionActionModel));
-
-    if (!err && data.success === true) {
-      this.paymentInstructionActionModel = new PaymentInstructionActionModel();
-      return this.router.navigateByUrl('/feelog');
     }
   }
 
@@ -229,5 +155,82 @@ export class FeelogeditComponent implements OnInit {
 
     return `£${(feeDetail.remission_amount / 100).toFixed(2)}`;
   }
+
+  goBack() { this.location.back(); }
+
+  onKeyUpFeeCodesAndDescriptions($ev) {
+    if ($ev.which === 13) { this.loadFeeCodesAndDescriptions(); }
+  }
+
+  async onProcessPaymentSubmission() {
+    this.paymentInstructionActionModel.action = PaymentAction.PROCESS;
+
+    const [err, data] = await UtilService
+      .toAsync(this.feeLogService.sendPaymentInstructionAction(this.model, this.paymentInstructionActionModel));
+
+    if (!err && data.success === true) {
+      this.paymentInstructionActionModel = new PaymentInstructionActionModel();
+      return this.router.navigateByUrl('/feelog');
+    }
+  }
+
+  async onSuspenseFormSubmit($event: Event) {
+    $event.preventDefault();
+
+    if (this.paymentInstructionActionModel.hasOwnProperty('reason')) {
+      const [err, data] = await UtilService
+        .toAsync(this.feeLogService.sendPaymentInstructionAction(this.model, this.paymentInstructionActionModel));
+
+      if (!err && data.success === true) {
+        this.paymentInstructionActionModel = new PaymentInstructionActionModel();
+        this.suspenseModalOn = !this.suspenseModalOn;
+        this.router.navigateByUrl('/feelog');
+      }
+    }
+  }
+
+  returnPaymentToPostClerk() {
+    this.model.status = PaymentStatus.DRAFT;
+    this.toggleReturnModal();
+    return this.router.navigateByUrl('/feelog');
+  }
+
+  selectFee(feeCodeModel) {
+    this.feeDetail.amount = this.getFeeAmount(feeCodeModel);
+    this.feeDetail.case_reference = this.currentCaseView.id;
+    this.feeDetail.case_reference_id = this.currentCaseView.id; // why this is here? i don't know (if we already have case_reference)
+    this.feeDetail.fee_code = feeCodeModel.code;
+    this.feeDetail.fee_description = feeCodeModel.current_version.description;
+    this.feeDetail.fee_version = feeCodeModel.current_version.version;
+  }
+
+  toggleAddRemissionBlock() { this.addRemissionOn = !this.addRemissionOn; }
+
+  toggleCaseModalWindow() { this.modalOn = !this.modalOn; }
+
+  toggleFeeDetailsModal(paymentInstructionCase?) {
+    this.currentCaseView = paymentInstructionCase;
+
+    if (this.feeDetailsModal) {
+      this.feeCodes = [];
+      this.searchFeeModel = '';
+      this.feeDetail.amount = null;
+
+      this.feeDetail.fee_code = '';
+      this.feeDetail.fee_description = '';
+      this.feeDetail.fee_version = '';
+
+      this.feeDetail.remission_amount = null;
+      this.feeDetail.remission_benefiter = '';
+      this.feeDetail.remission_authorisation = '';
+      this.addRemissionOn = false;
+    }
+
+    this.feeDetailsModal = !this.feeDetailsModal;
+  }
+
+  toggleReturnModal() { this.returnModalOn = !this.returnModalOn; }
+
+  toggleSuspenseModal() { this.suspenseModalOn = !this.suspenseModalOn; }
 
 }
