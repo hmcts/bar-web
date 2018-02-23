077583408724fe877a7c21502cef4af9d073fa44
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-02-23 14:37:01 +0000

    Submit (and change status to) "approved" done.

--- src/app/models/paymentstatus.model.ts
+++ src/app/models/paymentstatus.model.ts
@@ -1,6 +1,7 @@
 export class PaymentStatus {
+  static APPROVE = 'A';
   static DRAFT = 'D';
+  static PENDINGAPPROVAL = 'PA';
   static PENDING = 'P';
   static VALIDATED = 'V';
-  static PENDINGAPPROVAL = 'PA';
 }
