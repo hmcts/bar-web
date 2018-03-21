be82114ea92756b8cb94686859b8f7951eec64ad
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-03-21 16:57:47 +0000

    Remaining for Sachi

--- start.js
+++ start.js
@@ -1,7 +1,17 @@
 const app = require('./server').app,
+  config = require('config'),
   defaultPort = 3000,
-  port = process.env.PORT || defaultPort;
+  securePort = config.get('bar.securePort'),
+  port = process.env.PORT || defaultPort,
+  http = require('http'),
+  https = require('https');
 
+http.createServer(app).listen(port);
+
+// only needed for https
+https.createServer({
+  key: '',
+  cert: ''
+}, app).listen(securePort);
 // initialize the express app on the designated port
 // @TODO - Add logging (to indicate that this server is actually running)
-app.listen(port);
