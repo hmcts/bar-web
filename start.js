bee4da1b56ec6a8c3ddec4e8a68a547b5a6eb3ad
sachi <sachi.kuppuswami@hmcts.net>
2018-03-23 13:27:26 +0000

    BAR-229 implemented

--- start.js
+++ start.js
@@ -1,17 +1,20 @@
 const app = require('./server').app,
   config = require('config'),
   defaultPort = 3000,
-  securePort = config.get('bar.securePort'),
+  securePort = config.get('node.server.port'),
+  crtLocation = config.get('ssl.crt-location'),
+  keyLocation = config.get('ssl.key-location'),
   port = process.env.PORT || defaultPort,
   http = require('http'),
   https = require('https');
 
-http.createServer(app).listen(port);
+const key = fs.readFileSync(ctrLocation);
+const cert = fs.readFileSync(keyLocation);
 
-// only needed for https
+
 https.createServer({
-  key: '',
-  cert: ''
+  key: key,
+  cert: cert
 }, app).listen(securePort);
 // initialize the express app on the designated port
 // @TODO - Add logging (to indicate that this server is actually running)
