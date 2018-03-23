fa83a457be0d456a026264fdbc63df22bd1f0162
sachi <sachi.kuppuswami@hmcts.net>
2018-03-23 16:46:13 +0000

    BAR-229 fixed lint errors

--- start.js
+++ start.js
@@ -1,20 +1,16 @@
 const app = require('./server').app,
+  fs = require('fs'),
   config = require('config'),
-  defaultPort = 3000,
   securePort = config.get('node.server.port'),
   crtLocation = config.get('ssl.crt-location'),
   keyLocation = config.get('ssl.key-location'),
-  port = process.env.PORT || defaultPort,
-  http = require('http'),
   https = require('https');
 
-const key = fs.readFileSync(ctrLocation);
-const cert = fs.readFileSync(keyLocation);
+const key = fs.readFileSync(keyLocation);
+const cert = fs.readFileSync(crtLocation);
 
 
 https.createServer({
-  key: key,
-  cert: cert
+  key,
+  cert
 }, app).listen(securePort);
-// initialize the express app on the designated port
-// @TODO - Add logging (to indicate that this server is actually running)
