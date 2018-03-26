4f6c5ba4cf8f779624d19792175819c8e27ed791
sachi <sachi.kuppuswami@hmcts.net>
2018-03-26 17:49:54 +0100

    BAR-229 created certificates for development

--- start.js
+++ start.js
@@ -1,16 +1,18 @@
 const app = require('./server').app,
   fs = require('fs'),
+  path = require('path'),
   config = require('config'),
-  securePort = config.get('node.server.port'),
   crtLocation = config.get('ssl.crt-location'),
   keyLocation = config.get('ssl.key-location'),
+  defaultPort = 3000,
+  port = process.env.PORT || defaultPort,
   https = require('https');
 
-const key = fs.readFileSync(keyLocation);
-const cert = fs.readFileSync(crtLocation);
-
+const key = fs.readFileSync(path.join(__dirname, keyLocation));
+const cert = fs.readFileSync(path.join(__dirname, crtLocation));
 
 https.createServer({
   key,
   cert
-}, app).listen(securePort);
+}, app).listen(port);
+
