faf081c8d12c4605bd3a8d7b79bd792ef0aa03ed
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-03-26 17:19:17 +0100

    Dev certificates added.

--- start.js
+++ start.js
@@ -1,16 +1,16 @@
 const app = require('./server').app,
   fs = require('fs'),
+  path = require('path'),
+  DEFAULT_PORT = 3000,
   config = require('config'),
-  securePort = config.get('node.server.port'),
+  securePort = config.get('server.port') || DEFAULT_PORT,
   crtLocation = config.get('ssl.crt-location'),
   keyLocation = config.get('ssl.key-location'),
   https = require('https');
 
-const key = fs.readFileSync(keyLocation);
-const cert = fs.readFileSync(crtLocation);
+const key = fs.readFileSync(path.join(__dirname, keyLocation));
+const cert = fs.readFileSync(path.join(__dirname, crtLocation));
 
+https.createServer({ key, cert }, app).listen(securePort);
 
-https.createServer({
-  key,
-  cert
-}, app).listen(securePort);
+console.log('server started.');
