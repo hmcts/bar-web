7265272d9039a53092bd974a37d2c7fa469f60d8
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-03-29 16:53:36 +0100

    Latest changes. Fixed fees URI issue.

--- express/services/FeeService.js
+++ express/services/FeeService.js
@@ -3,20 +3,24 @@ const request = require('client-request/promise');
 
 const barUrl = config.get('bar.url');
 const feeUrl = config.has('fee.url') ? config.get('fee.url') : '';
+const fees = require('../../data/fees_search_results_response.json');
 
 class FeeService {
   constructor() {
     this.request = request;
-
     this.getFees = this.getFees.bind(this);
   }
 
+  // this would have to be amended later
   getFees() {
-    return this.request({
-      uri: `${feeUrl}`,
-      method: 'GET',
-      json: true
-    });
+    const body = fees;
+    return Promise.resolve({ body });
+
+    // return this.request({
+    //   uri: `${feeUrl}`,
+    //   method: 'GET',
+    //   json: true
+    // });
   }
 
   addEditFeeToCase(caseReferenceId, data, method = 'POST') {
@@ -42,9 +46,7 @@ class FeeService {
   removeFeeFromPaymentInstruction(caseFeeId) {
     return request({
       uri: `${barUrl}/fees/${caseFeeId}`,
-      method: 'DELETE',
-      json: true,
-      headers: { 'Content-Type': 'application/json' }
+      method: 'DELETE'
     });
   }
 }
