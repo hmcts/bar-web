30133f6e385a72fd850d680ee0592ca253319727
Attila Kiss <attila.kiss@hmcts.net>
2018-04-23 10:50:04 +0100

    [BAR-245][AK] retriggering build check

--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-
 import { ApprovedPaymentsComponent } from '../core/components/approved-payments/approved-payments.component';
 import { CheckSubmitComponent } from '../core/components/check-submit/check-submit.component';
 import { DashboardComponent } from '../core/components/dashboard/dashboard.component';
