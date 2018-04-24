5434f7f64db8fba5dcc2933b644a69cd513b78bd
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-04-24 12:42:56 +0100

    Latest changes.

---
+++ express/test/data/PaymentInstruction.js
@@ -0,0 +1,33 @@
+module.exports = {
+  paymentInstructionsList: [
+    {
+      payer_name: 'Derek Tchalakov',
+      amount: 19999,
+      currency: 'GBP',
+      id: 88,
+      status: 'Draft',
+      payment_date: '2017-12-07T10:28:02.167',
+      site_id: 'BR01',
+      daily_sequence_id: 2,
+      payment_type: {
+        id: 'cash',
+        name: 'Cash'
+      }
+    }
+  ],
+
+  paymentInstructionOne: {
+    payer_name: 'Derek Tchalakov',
+    amount: 19999,
+    currency: 'GBP',
+    id: 88,
+    status: 'Draft',
+    payment_date: '2017-12-07T10:28:02.167',
+    site_id: 'BR01',
+    daily_sequence_id: 2,
+    payment_type: {
+      id: 'cash',
+      name: 'Cash'
+    }
+  }
+};
