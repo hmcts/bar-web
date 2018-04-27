50c1165d336540b56e5b0663881a2b11b6ebbf87
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-04-27 17:23:07 +0100

    Unit test fix.

--- src/app/core/test-mocks/payment-log.service.mock.ts
+++ src/app/core/test-mocks/payment-log.service.mock.ts
@@ -1,7 +1,22 @@
 import {PaymentInstructionModel} from '../models/paymentinstruction.model';
 
 export class PaymentLogServiceMock {
-  getPaymentsLog(): Array<PaymentInstructionModel> {
-    return [];
+  getPaymentsLog(): Promise<PaymentInstructionModel[]> {
+    const paymentInstructionModels: PaymentInstructionModel[] = new Array<PaymentInstructionModel>();
+    return new Promise(resolve => resolve(paymentInstructionModels));
+  }
+
+  getPaymentById(feeId: number): PaymentInstructionModel {
+    return new PaymentInstructionModel();
+  }
+
+  getUnallocatedAmount(feeId: number) {
+    return new PaymentInstructionModel();
+  }
+
+  createCaseNumber(): Promise<PaymentInstructionModel[]> {
+    const paymentInstructionModels: PaymentInstructionModel[] = new Array<PaymentInstructionModel>();
+    return new Promise(resolve => resolve(paymentInstructionModels));
   }
 }
+
