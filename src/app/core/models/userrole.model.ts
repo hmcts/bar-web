522aa9f4dbfcfecaef5ab861dcdfb8759461b151
rtkumar1980 <34369731+rtkumar1980@users.noreply.github.com>
2018-05-03 15:58:29 +0100

    Added payment instruction stats function (#118)
    * Added payment instruction stats function
    
    * Fixed broken test.
    
    * Fixed code for failing tests

---
+++ src/app/core/models/userrole.model.ts
@@ -0,0 +1,6 @@
+export class UserRole {
+    static FEECLERK = 'bar-fee-clerk';
+    static POSTCLERK = 'bar-post-clerk';
+    static SRFEECLERK = 'bar-senior-clerk';
+    static DELIVERYMANAGER = 'bar-delivery-manager';
+}
