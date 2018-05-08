d2750a43ed0e241eb5458163d2f8f6c6d529a2a2
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-05-08 12:30:40 +0100

    Latest changes made and tests added.

--- express/services/BaseService.js
+++ express/services/BaseService.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-eq-null, eqeqeq */
 const request = require('client-request/promise');
 const constants = require('../infrastructure/security').constants;
+const validator = require('validator');
 
 class BaseService {
   /**
@@ -13,6 +14,18 @@ class BaseService {
   }
 
   configureOpts(opts, req) {
+    if (!opts.hasOwnProperty('uri') || !opts.hasOwnProperty('method')) {
+      throw new Error('"uri" and "method" should contain data.');
+    }
+
+    if (opts.uri.length < 1 || opts.method.length < 1) {
+      throw new Error('"uri" and "method" should not be blank');
+    }
+
+    if (typeof opts !== 'object' || req && typeof req !== 'object') {
+      throw new Error('Please ensure "opts" and "request" are of type "Object".')
+    }
+
     opts.json = opts.json == null ? true : opts.json;
     if (!opts.headers) {
       opts.headers = {};
--- express/test/services/BaseService.test.js
+++ express/test/services/BaseService.test.js
@@ -8,16 +8,27 @@ const { expect } = chai;
 const BaseService = require('../../services/BaseService');
 
 let BaseServiceMock = null;
+let mockBarUrl = null;
 
 // start tests
 describe('Test: BaseService.test.js', () => {
   beforeEach(() => {
+    mockBarUrl = 'http://localhost:80';
     BaseServiceMock = new BaseService();
   });
 
-  // ensure we get payment types
-  it('Should remove json property from object if method type is DELETE', () => {
-    const mockBarUrl = 'http://localhost:80';
+  it('Should ensure that uri and method exist and aren\'t blank or empty.', () => {
+    const mockOptions = { uri: '', method: '' };
+    expect(() => BaseServiceMock.configureOpts(mockOptions)).to.throw(Error);
+  });
+
+  it('Should ensure that both arguments are objects.', () => {
+    const mockOptions = 1;
+    const mockRequest = 2;
+    expect(() => BaseServiceMock.configureOpts(mockOptions, mockRequest)).to.throw(Error);
+  });
+
+  it('Should remove json property from object if method type is DELETE.', () => {
     const mockOptions = { uri: `${mockBarUrl}/payment-instructions`, method: 'DELETE' };
     const generatedOptions = BaseServiceMock.configureOpts(mockOptions);
     expect(generatedOptions.json).to.equal(false);
