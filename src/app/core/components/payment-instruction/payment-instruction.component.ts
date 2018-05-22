82533993927307adb83887c9469fb6ab46955d27
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-05-22 13:36:32 +0100

    Latest changes

--- src/app/core/components/payment-instruction/payment-instruction.component.ts
+++ src/app/core/components/payment-instruction/payment-instruction.component.ts
@@ -9,180 +9,139 @@ import { PaymentInstructionModel } from '../../models/paymentinstruction.model';
 import 'rxjs/add/operator/switchMap';
 import { PaymentStatus } from '../../models/paymentstatus.model';
 import {UserModel} from '../../models/user.model';
+import * as _ from 'lodash';
+import { PaymentInstructionsService } from '../../services/payment-instructions/payment-instructions.service';
 
 @Component({
   selector: 'app-payment-instruction',
   templateUrl: './payment-instruction.component.html',
-  providers: [PaymentslogService, PaymenttypeService],
+  providers: [PaymentInstructionsService, PaymentslogService, PaymenttypeService],
   styleUrls: ['./payment-instruction.component.scss']
 })
 export class PaymentInstructionComponent implements OnInit {
   model: PaymentInstructionModel = new PaymentInstructionModel();
   paymentTypes: IPaymentType[] = [];
-  filledContent = false;
   showModal = false;
   newDataId = 0;
-  loadedId = null;
   changePayment = false;
 
   constructor(
+    private _paymentInstructionService: PaymentInstructionsService,
     private paymentTypeService: PaymenttypeService,
     private paymentLogService: PaymentslogService,
     private userService: UserService,
     private router: Router,
     private route: ActivatedRoute,
-    private location: Location
+    public location: Location
   ) { }
 
-  async ngOnInit() {
-    // load payment types
-    await this.loadPaymentTypes();
-
-    // subscribe to the paymenttypes list
-    this.paymentTypeService
-      .paymentTypesSource
-      .subscribe(payments => this.paymentTypes = payments);
-
-    this.route
-      .params
-      .subscribe(params => this.onRouteParams(params));
+  ngOnInit() {
+    this.route.params.subscribe(params => this.onRouteParams(params));
+    this.getPaymentTypes();
   }
 
-  onRouteParams(params) {
-    if (typeof params.id !== 'undefined') {
-      this.loadedId = params.id;
-      if (/[0-9]/.test(this.loadedId)) {
-        if (this.router.url.includes('/change-payment')) {
-          this.changePayment = true;
-        }
-        this.loadPaymentDataById(this.loadedId);
-      } else {
-        return this.router.navigateByUrl('/paymentslog');
+  get cleanModel(): PaymentInstructionModel {
+    const model = new PaymentInstructionModel;
+    Object.keys(this.model).forEach(key => (this.model[key] !== '') ? model[key] = this.model[key] : null);
+    return model;
+  }
+
+  get hasPopulatedField(): boolean {
+    return (Object.keys(this.model).filter(key => {
+      if (key === 'currency' || key === 'unallocated_amount' || key === 'payment_type') {
+        return false;
       }
-    }
+      return this.model[key].length > 0;
+    }).length > 0);
   }
 
-  onFormSubmission() {
-    const { type } = this.userService.getUser();
-
-    this.paymentTypeService
-      .savePaymentModel(this.model)
-      .then(response => {
-        this.resetData();
-
-        if (response.data !== null) {
-          this.newDataId = response.data.daily_sequence_id;
-          if (typeof this.model.id === 'undefined') {
-            this.showModal = true;
-
-            if (type === UserModel.TYPES.feeclerk.type) {
-              this.model = response.data;
-              this.model.status = PaymentStatus.PENDING;
-              this.onFormSubmission();
-              return;
-            }
-          }
-        }
-
-        if (type === UserModel.TYPES.feeclerk.type) {
-          return this.router.navigateByUrl(`/feelog/edit/${this.model.id}`);
-        }
-        return this.router.navigateByUrl('/paymentslog');
+  getPaymentInstructionById(paymentID) {
+    this.paymentLogService.getPaymentById(paymentID)
+      .then((response: IResponse) => {
+        this.model = response.data;
+        this.model.payment_type = this.model.payment_type.id;
       })
-    .catch(err => console.log(err));
+      .catch(err => console.log(err));
   }
 
-  onInputPropertyChange($ev): void {
-    // check if all the fields are empty or not
-    if (this.hasPopulatedField()) {
-      this.filledContent = true;
-    } else {
-      this.filledContent = false;
-    }
+  getPaymentTypes() {
+    this.paymentTypeService.getPaymentTypes()
+      .then((response: IResponse) => this.paymentTypes = response.data.map(paymentType => ({ id: paymentType.id, name: paymentType.name })))
+      .catch(err => console.log(err));
   }
 
-  onToggleShowModal(): void {
-    this.showModal = false;
-    this.newDataId = 0;
-  }
-
-  redirectBackPaymentLog() {
-    this.location.back();
-  }
-
-  private async loadPaymentDataById(paymentID) {
-    try {
-      const response = await this.paymentLogService.getPaymentById(paymentID);
-      this.model = response.data;
-      this.model.payment_type = this.model.payment_type.id;
-    } catch (exception) {
-      console.log( exception );
-    }
-  }
-
-  private hasPopulatedField(): boolean {
-    let hasPopulatedField = false;
-
-    for (const property in this.model) {
-      if (this.model.hasOwnProperty(property)) {
-        if (property === 'currency' || property === 'payment_type') {
-          continue;
-        }
-
-        if (this.model[property].length > 0) {
-          hasPopulatedField = true;
-          break;
-        }
-      }
-    }
-
-    return hasPopulatedField;
-  }
-
-  private async loadPaymentTypes() {
-    try {
-      const paymentTypes: any = await this.paymentTypeService.getPaymentTypes();
-      if (paymentTypes.success === true) {
-        const paymentTypesBatch: IPaymentType[] = [];
-        for (let i = 0; i < paymentTypes.data.length; i++) {
-          const paymentType: IPaymentType = {
-            id: paymentTypes.data[i].id,
-            name: paymentTypes.data[i].name
-          };
-          paymentTypesBatch.push(paymentType);
-        }
-        this.paymentTypeService.setPaymentTypeList( paymentTypesBatch );
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  private cleanData(): {} {
-    const cleanData = {};
-    for (const property in this.model) {
-      if (
-        this.model.hasOwnProperty(property) &&
-        this.model[property] !== ''
-      ) {
-        cleanData[property] = this.model[property];
-      }
-    }
-
-    return cleanData;
-  }
-
-  private resetData(): void {
-    if (!this.loadedId) {
+  resetData() {
+    if (!this.model.id) {
       this.model.amount = null;
       this.model.payer_name = '';
     }
-
     this.model.all_pay_transaction_id = '';
-
+    this.model.authorization_code = '';
     this.model.cheque_number = '';
     this.model.postal_order_number = '';
+  }
 
-    this.model.authorization_code = '';
+  // events go below here
+  onFormSubmission(e?) {
+    if (e) {
+      e.preventDefault();
+    }
+
+    const { type } = this.userService.getUser();
+    this._paymentInstructionService
+      .savePaymentInstruction(this.cleanModel)
+      .subscribe(
+        (response: IResponse) => {
+          console.log( response );
+          if (
+            (response.data.hasOwnProperty('status') && response.data.status === PaymentStatus.DRAFT) &&
+            type === UserModel.TYPES.feeclerk.type
+          ) {
+            this.model = response.data;
+            this.model.status = PaymentStatus.PENDING;
+            this.onFormSubmission();
+          }
+          this.resetData();
+        },
+        err => console.log(err)
+      );
+
+    // this.paymentTypeService
+    //   .savePaymentModel(this.model)
+    //   .then(response => {
+    //     this.resetData();
+
+    //     if (response.data !== null) {
+    //       this.newDataId = response.data.daily_sequence_id;
+    //       if (typeof this.model.id === 'undefined') {
+    //         this.showModal = true;
+
+    //         if (type === UserModel.TYPES.feeclerk.type) {
+    //           this.model = response.data;
+    //           this.model.status = PaymentStatus.PENDING;
+    //           this.onFormSubmission();
+    //           return;
+    //         }
+    //       }
+    //     }
+
+    //     if (type === UserModel.TYPES.feeclerk.type) {
+    //       return this.router.navigateByUrl(`/feelog/edit/${this.model.id}`);
+    //     }
+    //     return this.router.navigateByUrl('/paymentslog');
+    //   })
+    // .catch(err => console.log(err));
+  }
+
+  onRouteParams(params) {
+    if (params.id && /[0-9]/.test(params.id)) {
+      this.getPaymentInstructionById(params.id);
+      this.changePayment = (this.router.url.includes('/change-payment'));
+    }
+  }
+
+  onToggleShowModal() {
+    this.showModal = false;
+    this.newDataId = 0;
   }
 }
