00ce77a805b7b1c17f5447abdef6ab2c337f1890
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-05-25 06:50:08 +0100

    Tests added (75% coverage)

--- src/app/core/models/feedetail.model.spec.ts
+++ src/app/core/models/feedetail.model.spec.ts
@@ -46,4 +46,29 @@ describe('check feedetail method', () => {
 
   });
 
+  it('test to ensure all fields are empty after reset', () => {
+    const feeDetailModel = new FeeDetailModel();
+    feeDetailModel.amount = null;
+    feeDetailModel.fee_code = '';
+    feeDetailModel.fee_description = '';
+    feeDetailModel.fee_version = '';
+    feeDetailModel.remission_amount = null;
+    feeDetailModel.remission_benefiter = '';
+    feeDetailModel.remission_authorisation = '';
+    feeDetailModel.refund_amount = 23.99;
+    feeDetailModel.reset();
+
+    const feeDetailModelBlank = new FeeDetailModel();
+    feeDetailModelBlank.amount = null;
+    feeDetailModelBlank.fee_code = '';
+    feeDetailModelBlank.fee_description = '';
+    feeDetailModelBlank.fee_version = '';
+    feeDetailModelBlank.remission_amount = null;
+    feeDetailModelBlank.remission_benefiter = '';
+    feeDetailModelBlank.remission_authorisation = '';
+    feeDetailModelBlank.refund_amount = null;
+
+    expect(feeDetailModel.absEquals(feeDetailModelBlank)).toBe(true);
+  });
+
 });
