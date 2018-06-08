1c2ea5d29c1af4c49405837b44bc3b9e224ace91
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-06-08 17:15:12 +0100

    Bar 289 (#173)
    * Latest changes
    
    * Latest changes

--- src/app/core/components/payment-instruction/payment-instruction.component.spec.ts
+++ src/app/core/components/payment-instruction/payment-instruction.component.spec.ts
@@ -1,33 +1,34 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
-import { PaymentInstructionComponent } from './payment-instruction.component';
-import { ModalComponent } from './../modal/modal.component';
+import {PaymentInstructionComponent} from './payment-instruction.component';
+import {ModalComponent} from './../modal/modal.component';
 
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { HttpClientModule } from '@angular/common/http';
-import { ActivatedRoute, ParamMap, Router, RouterModule, RouterLinkWithHref } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
+import {ActivatedRoute, ParamMap, Router, RouterModule, RouterLinkWithHref} from '@angular/router';
 
-import { UserService } from '../../../shared/services/user/user.service';
-import { PaymenttypeService } from '../../services/paymenttype/paymenttype.service';
+import {UserService} from '../../../shared/services/user/user.service';
+import {PaymenttypeService} from '../../services/paymenttype/paymenttype.service';
 
-import { Observable } from 'rxjs/Observable';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import {Observable} from 'rxjs/Observable';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
-import { NumbersOnlyDirective } from '../../directives/numbers-only.directive';
-import { CookieService } from 'ngx-cookie-service';
-import { PaymentTypeServiceMock } from '../../test-mocks/payment-type.service.mock';
-import { UserServiceMock } from '../../test-mocks/user.service.mock';
-import { PaymentslogService } from '../../services/paymentslog/paymentslog.service';
-import { PaymentLogServiceMock } from '../../test-mocks/payment-log.service.mock';
-import { PaymentInstructionsService } from '../../services/payment-instructions/payment-instructions.service';
-import { PaymentInstructionServiceMock } from '../../test-mocks/payment-instruction.service.mock';
-import { By } from '@angular/platform-browser';
-import { UserModel } from '../../models/user.model';
-import { createPaymentInstruction } from '../../../test-utils/test-utils';
-import { PaymentTypeModel } from '../../models/paymenttype.model';
-import { PaymentInstructionModel } from '../../models/paymentinstruction.model';
+import {NumbersOnlyDirective} from '../../directives/numbers-only.directive';
+import {CookieService} from 'ngx-cookie-service';
+import {PaymentTypeServiceMock} from '../../test-mocks/payment-type.service.mock';
+import {UserServiceMock} from '../../test-mocks/user.service.mock';
+import {PaymentslogService} from '../../services/paymentslog/paymentslog.service';
+import {PaymentLogServiceMock} from '../../test-mocks/payment-log.service.mock';
+import {PaymentInstructionsService} from '../../services/payment-instructions/payment-instructions.service';
+import {PaymentInstructionServiceMock} from '../../test-mocks/payment-instruction.service.mock';
+import {By} from '@angular/platform-browser';
+import {UserModel} from '../../models/user.model';
+import {createPaymentInstruction} from '../../../test-utils/test-utils';
+import {PaymentTypeModel} from '../../models/paymenttype.model';
+import {PaymentInstructionModel} from '../../models/paymentinstruction.model';
+import {PaymentStatus} from '../../models/paymentstatus.model';
 
 describe('PaymentInstructionComponent', () => {
   let component: PaymentInstructionComponent;
@@ -40,6 +41,7 @@ describe('PaymentInstructionComponent', () => {
     get url() {
       return '/change-payment';
     }
+
     events() {
       return new Observable(ob => {
         ob.next({});
@@ -47,7 +49,9 @@ describe('PaymentInstructionComponent', () => {
       });
     }
 
-    navigateByUrl(url: string) { return url; }
+    navigateByUrl(url: string) {
+      return url;
+    }
   }
 
   class MockActivatedRoute {
@@ -55,7 +59,7 @@ describe('PaymentInstructionComponent', () => {
     get params() {
       return new Observable(observer => {
         observer.next({id: '2'}),
-        observer.complete();
+          observer.complete();
       });
     }
   }
@@ -63,16 +67,16 @@ describe('PaymentInstructionComponent', () => {
   beforeEach(async(() => {
 
     TestBed.configureTestingModule({
-      imports: [ FormsModule, HttpModule, HttpClientModule, RouterModule, RouterTestingModule.withRoutes([]) ],
-      declarations: [ PaymentInstructionComponent, ModalComponent, NumbersOnlyDirective ]
+      imports: [FormsModule, HttpModule, HttpClientModule, RouterModule, RouterTestingModule.withRoutes([])],
+      declarations: [PaymentInstructionComponent, ModalComponent, NumbersOnlyDirective]
     }).overrideComponent(PaymentInstructionComponent, {
       set: {
         providers: [
-          { provide: PaymenttypeService, useClass: PaymentTypeServiceMock },
-          { provide: PaymentInstructionsService, useClass: PaymentInstructionServiceMock },
-          { provide: UserService, useClass: UserServiceMock },
-          { provide: Router, useClass: MockRouter },
-          { provide: ActivatedRoute, useClass: MockActivatedRoute }
+          {provide: PaymenttypeService, useClass: PaymentTypeServiceMock},
+          {provide: PaymentInstructionsService, useClass: PaymentInstructionServiceMock},
+          {provide: UserService, useClass: UserServiceMock},
+          {provide: Router, useClass: MockRouter},
+          {provide: ActivatedRoute, useClass: MockActivatedRoute}
         ]
       }
     });
@@ -177,6 +181,33 @@ describe('PaymentInstructionComponent', () => {
     component.onFormSubmission();
     expect(component.newId).toEqual(component.model.id);
     expect(component.newDailySequenceId).toEqual(component.model.daily_sequence_id);
+  });
+
+  it('should add "all pay" payment instruction.', () => {
+    component.model = createPaymentInstruction();
+    const paymentTypeModel = new PaymentTypeModel();
+    paymentTypeModel.id = 'allpay';
+    paymentTypeModel.name = 'AllPay';
+
+    component.model.payment_type = paymentTypeModel;
+    component.onFormSubmission();
+    expect(component.newId).toEqual(component.model.id);
+    expect(component.newDailySequenceId).toEqual(component.model.daily_sequence_id);
+  });
+
+  it('should add "draft" payment based on user', () => {
+    const paymentTypeModel = new PaymentTypeModel();
+    paymentTypeModel.id = 'cash';
+    paymentTypeModel.name = 'Cash';
+
+    component.model = createPaymentInstruction();
+    component.model.payment_type = paymentTypeModel;
+
+    // change the status to draft (and then test to see if it will be changed to "Pending"
+    component.model.status = PaymentStatus.getPayment('Draft').code;
+
+    component.onFormSubmission();
+    expect(component.model.status).toBeTruthy(PaymentStatus.getPayment('Pending').code);
   });
 
 });
