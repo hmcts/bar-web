d10df7abefe689d8faf7215494f73a07e0d2ce60
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-06-18 12:21:33 +0100

    Tests added, and some reformatted

--- express/mvc/controllers/index.js
+++ express/mvc/controllers/index.js
@@ -4,9 +4,12 @@ const FeeController = require('./FeeController');
 const PaymentInstructionController = require('./PaymentInstructionController');
 const PaymentsOverviewController = require('../controllers/PaymentsOverviewController');
 
+// --- services ---
+const { paymentsLogService } = require('./../../services');
+
 module.exports = {
   paymentsController: new PaymentsController(),
-  paymentsLogController: new PaymentsLogController(),
+  paymentsLogController: new PaymentsLogController({ paymentsLogService }),
   feesController: new FeeController(),
   paymentInstructionController: new PaymentInstructionController(),
   paymentsOverviewController: new PaymentsOverviewController()
