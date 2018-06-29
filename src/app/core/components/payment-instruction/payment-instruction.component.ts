abb42e3017c709ecfcae606ff689bac499480af2
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-06-29 14:35:34 +0100

    Latest changes.

--- src/app/core/components/payment-instruction/payment-instruction.component.ts
+++ src/app/core/components/payment-instruction/payment-instruction.component.ts
@@ -76,17 +76,6 @@ export class PaymentInstructionComponent implements OnInit {
     return this._userService.getUser();
   }
 
-  get everyFieldIsFilled(): boolean {
-    return (_
-      .chain(Object.keys(this.model))
-      .reject(key => (key === 'currency') || (key === 'unallocated_amount') || (key === 'payment_type') || (key === 'case_fee_details'))
-      .map(key => this.model[key])
-      .filter((value) => (_.isNull(value) || _.isEqual(value, '') || (_.isString(value) && value.trim().length < 1))
-        ? true
-        : false)
-      .value().length > 0) ? false : true;
-  }
-
   // ------------------------------------------------------------------------------------------
   addAnotherPayment() {
     this.model = new PaymentInstructionModel();
