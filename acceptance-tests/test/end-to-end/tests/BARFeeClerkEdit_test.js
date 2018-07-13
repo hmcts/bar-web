3ef62f4a336eced1ac660ad3ffb23ef976b7c4cf
Your Name <you@example.com>
2018-07-13 14:17:16 +0100

    Add acceptance tests to pipeline

---
+++ acceptance-tests/test/end-to-end/tests/BARFeeClerkEdit_test.js
@@ -0,0 +1,55 @@
+const BARATConstants = require('./BARAcceptanceTestConstants');
+
+Feature('BAR Fee Clerk Edit and Remove Payment Instruction');
+
+Before(I => {
+  I.amOnPage('/');
+  I.resizeWindow(BARATConstants.windowsSizeX, BARATConstants.windowsSizeY);
+  I.login('i119419@nwytg.com', 'LevelAt12');
+  I.wait(BARATConstants.fiveSecondWaitTime);
+  I.click('Add payment information');
+});
+
+/* Scenario('Add Payment Instruction', (I) => {
+
+    I.see('Add Payment Instruction')
+    I.see('Payment Type')
+    I.waitForElement({css: '[type="radio"]'}, 30)
+    I.see('Cheque')
+    I.see('Cash')
+    I.see('Postal Order')
+    I.see('AllPay')
+    I.see('Card')
+    I.see('Payer name')
+    I.see('Amount')
+    I.seeElement('.button.button-view:disabled');
+})
+*/
+Scenario('Select Payment Type Cheque', I => {
+  I.feeclerkEditChequePaymentType();
+});
+
+
+/* Scenario('View Payments Log Validations', (I) => {
+    I.seeElement('.button.button-blue');
+I.click({css: '#payment-type-1'})
+I.click({css: '#payment-type-2'})
+I.click({css: '#payment-type-3'})
+I.click({css: '#payment-type-4'})
+I.click({css: '#payment-type-5'})
+I.click({css: '#payment-type-6'})
+I.seeElement('.button.button-blue.view-payment-log-disabled');
+})
+
+Scenario('View Payments Log Validations With PayeeName', (I) => {
+    I.seeElement('.button.button-blue');
+I.fillField('#payee-name', 'john');
+//I.fillField('Amount*', '123456');
+I.seeElement('.button.button-blue.view-payment-log-disabled');
+})
+
+Scenario('View Payments Log Validations With Amount', (I) => {
+    I.seeElement('.button.button-blue');
+I.fillField('#amount', '123456');
+I.seeElement('.button.button-blue.view-payment-log-disabled');
+})*/
---
+++ acceptance-tests/test/end-to-end/tests/BARPostClerkEdit_test.js
@@ -0,0 +1,41 @@
+const BARATConstants = require('./BARAcceptanceTestConstants');
+
+Feature('BAR Post Clerk Edit Payment Instruction');
+
+Before(I => {
+  I.amOnPage('/');
+  I.resizeWindow(BARATConstants.windowsSizeX, BARATConstants.windowsSizeY);
+  I.login('i118030@nwytg.com', 'LevelAt12');
+});
+
+Scenario('Select Payment Type Card', I => {
+  I.waitForText('Add Payment Instruction', BARATConstants.tenSecondWaitTime);
+  I.paymentTypeCard();
+});
+
+Scenario('Edit Card Payment', I => {
+  I.editPayerNameAmountAndAuthorizationCode();
+});
+/* Scenario('View Payments Log Validations', (I) => {
+    I.seeElement('.button.button-blue');
+I.click({css: '#payment-type-1'})
+I.click({css: '#payment-type-2'})
+I.click({css: '#payment-type-3'})
+I.click({css: '#payment-type-4'})
+I.click({css: '#payment-type-5'})
+I.click({css: '#payment-type-6'})
+I.seeElement('.button.button-blue.view-payment-log-disabled');
+})
+
+Scenario('View Payments Log Validations With PayeeName', (I) => {
+    I.seeElement('.button.button-blue');
+I.fillField('#payee-name', 'john');
+//I.fillField('Amount*', '123456');
+I.seeElement('.button.button-blue.view-payment-log-disabled');
+})
+
+Scenario('View Payments Log Validations With Amount', (I) => {
+    I.seeElement('.button.button-blue');
+I.fillField('#amount', '123456');
+I.seeElement('.button.button-blue.view-payment-log-disabled');
+})*/
