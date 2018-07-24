34fe2e61f700364ea39b9725e8bbb0717027d161
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-07-24 15:50:58 +0100

    UserService test fixed.

--- src/app/core/components/login-form/login-form.component.ts
+++ src/app/core/components/login-form/login-form.component.ts
@@ -9,124 +9,141 @@ import { UserModel } from '../../models/user.model';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
-  @Output() onAuthenticated: EventEmitter<boolean> = new EventEmitter;
+  @Output() onAuthenticated: EventEmitter<boolean> = new EventEmitter();
   users: UserModel[] = new Array<UserModel>();
   model: LoginFormModel;
 
   constructor(private _userService: UserService) {}
 
   ngOnInit() {
-    this.users.push(new UserModel({
-      id: 365750,
-      courtId: 'BR01',
-      email: 'post.clerk@hmcts.net',
-      forename: 'Chris',
-      surname: 'Spencer',
-      password: 'LevelAt12',
-      roles: ['bar-post-clerk']
-    }));
+    this.users.push(
+      new UserModel({
+        id: 365750,
+        courtId: 'BR01',
+        email: 'post.clerk@hmcts.net',
+        forename: 'Chris',
+        surname: 'Spencer',
+        password: 'password',
+        roles: ['bar-post-clerk']
+      })
+    );
 
-    this.users.push(new UserModel({
-      id: 365751,
-      courtId: 'BR02',
-      email: 'fee.clerk@hmcts.net',
-      forename: 'Karen',
-      surname: 'Taylor',
-      password: 'LevelAt12',
-      roles: ['bar-fee-clerk']
-    }));
+    this.users.push(
+      new UserModel({
+        id: 365751,
+        courtId: 'BR02',
+        email: 'fee.clerk@hmcts.net',
+        forename: 'Karen',
+        surname: 'Taylor',
+        password: 'password',
+        roles: ['bar-fee-clerk']
+      })
+    );
 
-    this.users.push(new UserModel({
-      id: 365752,
-      courtId: 'BR03',
-      email: 'seniorfee.clerk@hmcts.net',
-      forename: 'James',
-      surname: 'Black',
-      password: 'LevelAt12',
-      roles: ['bar-senior-clerk']
-    }));
+    this.users.push(
+      new UserModel({
+        id: 365752,
+        courtId: 'BR03',
+        email: 'seniorfee.clerk@hmcts.net',
+        forename: 'James',
+        surname: 'Black',
+        password: 'password',
+        roles: ['bar-senior-clerk']
+      })
+    );
 
-    this.users.push(new UserModel({
-      id: 365753,
-      courtId: 'BR04',
-      email: 'delivery.manager@hmcts.net',
-      forename: 'Dee',
-      surname: 'Aliu',
-      password: 'LevelAt12',
-      roles: ['bar-delivery-manager']
-    }));
+    this.users.push(
+      new UserModel({
+        id: 365753,
+        courtId: 'BR04',
+        email: 'delivery.manager@hmcts.net',
+        forename: 'Dee',
+        surname: 'Aliu',
+        password: 'password',
+        roles: ['bar-delivery-manager']
+      })
+    );
 
-    this.users.push(new UserModel({
-      id: 365756,
-      courtId: 'BR03',
-      email: 'seniorfee.clerk2@hmcts.net',
-      forename: 'James2',
-      surname: 'Black2',
-      password: 'LevelAt12',
-      roles: ['bar-senior-clerk']
-    }));
+    this.users.push(
+      new UserModel({
+        id: 365756,
+        courtId: 'BR03',
+        email: 'seniorfee.clerk2@hmcts.net',
+        forename: 'James2',
+        surname: 'Black2',
+        password: 'password',
+        roles: ['bar-senior-clerk']
+      })
+    );
 
-    this.users.push(new UserModel({
-      id: 365757,
-      courtId: 'BR03',
-      email: 'barpreprodpostclerk@mailinator.com',
-      forename: 'Anjani',
-      surname: 'PostClerk',
-      password: 'LevelAt12',
-      roles: ['bar-post-clerk']
-    }));
+    this.users.push(
+      new UserModel({
+        id: 365757,
+        courtId: 'BR03',
+        email: 'barpreprodpostclerk@mailinator.com',
+        forename: 'Anjani',
+        surname: 'PostClerk',
+        password: 'password',
+        roles: ['bar-post-clerk']
+      })
+    );
 
-    this.users.push(new UserModel({
-      id: 365758,
-      courtId: 'BR02',
-      email: 'barpreprodfeeclerk@mailinator.com',
-      forename: 'Anish',
-      surname: 'feeclerk',
-      password: 'LevelAt12',
-      roles: ['bar-fee-clerk']
-    }));
+    this.users.push(
+      new UserModel({
+        id: 365758,
+        courtId: 'BR02',
+        email: 'barpreprodfeeclerk@mailinator.com',
+        forename: 'Anish',
+        surname: 'feeclerk',
+        password: 'password',
+        roles: ['bar-fee-clerk']
+      })
+    );
 
-    this.users.push(new UserModel({
-      id: 365759,
-      courtId: 'BR03',
-      email: 'barpreprodsrfeeclerk@mailinator.com',
-      forename: 'krishna',
-      surname: 'Srfeeclerk',
-      password: 'LevelAt12',
-      roles: ['bar-senior-clerk']
-    }));
+    this.users.push(
+      new UserModel({
+        id: 365759,
+        courtId: 'BR03',
+        email: 'barpreprodsrfeeclerk@mailinator.com',
+        forename: 'krishna',
+        surname: 'Srfeeclerk',
+        password: 'password',
+        roles: ['bar-senior-clerk']
+      })
+    );
 
-    this.users.push(new UserModel({
-      id: 365760,
-      courtId: 'BR04',
-      email: 'barpreprod@mailinator.com',
-      forename: 'PreProd',
-      surname: 'Admin',
-      password: 'LevelAt12',
-      roles: ['bar-delivery-manager']
-    }));
+    this.users.push(
+      new UserModel({
+        id: 365760,
+        courtId: 'BR04',
+        email: 'barpreprod@mailinator.com',
+        forename: 'PreProd',
+        surname: 'Admin',
+        password: 'password',
+        roles: ['bar-delivery-manager']
+      })
+    );
 
-    this.model = new LoginFormModel( this.users[0] );
+    this.model = new LoginFormModel(this.users[0]);
   }
 
   onSubmit($ev) {
     $ev.preventDefault();
     this.findUser(this.model.email);
-    const authenticate = this._userService.authenticate( this.model.getUser() );
+    const authenticate = this._userService.authenticate(this.model.getUser());
     this.onAuthenticated.emit(authenticate);
   }
 
   findUser(email: string) {
     for (let i = 0; i < this.users.length; i++) {
       if (this.users[i].email === email) {
-        this.model = new LoginFormModel( this.users[i] );
+        this.model = new LoginFormModel(this.users[i]);
         break;
       }
     }
   }
 
   selectUser(userModel: UserModel) {
-    this.model = new LoginFormModel( userModel );
+    this.model = new LoginFormModel(userModel);
   }
-
 }
