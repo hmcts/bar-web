f1bed82b932190124ce2136aca201b0a9b4f07b3
Your Name <you@example.com>
2018-07-27 17:12:06 +0100

    Updated the logic of rejected PI retrieval

--- src/app/core/services/paymentslog/paymentslog.service.ts
+++ src/app/core/services/paymentslog/paymentslog.service.ts
@@ -30,11 +30,11 @@ export class PaymentslogService {
     if (typeof searchModel.status !== 'undefined') {
       params = `?status=${searchModel.status}`;
     }
-    if (typeof searchModel.oldStatus !== 'undefined') {
-      params += `&oldStatus=${searchModel.oldStatus}`;
-    }
     if (typeof searchModel.paymentType !== 'undefined') {
       params += `&paymentType=${searchModel.paymentType}`;
+    }
+    if (typeof searchModel.piIds !== 'undefined') {
+      params += `?piIds=${searchModel.piIds}`;
     }
     endPoint = `${environment.apiUrl}/users/${searchModel.id}/payment-instructions${params}`;
     return this.http
