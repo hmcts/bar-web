5934492b53ad7a1067d741f4aa591a47abd8d011
Your Name <you@example.com>
2018-08-09 16:40:57 +0100

    Revamped functional tests

--- acceptance-tests/test/end-to-end/tests/BARPostClerk_test.js
+++ acceptance-tests/test/end-to-end/tests/BARPostClerk_test.js
@@ -5,10 +5,10 @@ Feature('BAR Post Clerk Add Payment Instruction');
 BeforeSuite(I => {
   I.amOnPage('/');
   I.resizeWindow(BARATConstants.windowsSizeX, BARATConstants.windowsSizeY);
-  I.login('barpreprodpostclerk@mailinator.com', 'LevelAt12');
 });
 
 Scenario('Add Payment Instruction', I => {
+  I.login('barpreprodpostclerk@mailinator.com', 'LevelAt12');
   I.waitForText('Add Payment Instruction', BARATConstants.tenSecondWaitTime);
   I.see('Add Payment Instruction');
   I.see('Payment Type');
@@ -28,31 +28,18 @@ Scenario('Select Payment Type Cheque', I => {
 });
 
 Scenario('Select Payment Type Postal Order', I => {
-  I.wait(BARATConstants.twoSecondWaitTime);
-  I.click({ xpath: '//div[2]/div/ul/li[1]/a' });
   I.paymentTypePostalOrder();
 });
 
 Scenario('Select Payment Type Cash', I => {
-  I.wait(BARATConstants.twoSecondWaitTime);
-  I.click({ xpath: '//div[2]/div/ul/li[1]/a' });
   I.paymentTypeCash();
 });
 
 Scenario('Select Payment Type All Pay', I => {
-  I.wait(BARATConstants.twoSecondWaitTime);
-  I.click({ xpath: '//div[2]/div/ul/li[1]/a' });
   I.paymentTypeAllPay();
 });
 
 Scenario('Select Payment Type Card', I => {
-  I.wait(BARATConstants.twoSecondWaitTime);
-  I.click({ xpath: '//div[2]/div/ul/li[1]/a' });
   I.paymentTypeCard();
-});
-
-Scenario('Submit Card Payment', I => {
-  I.wait(BARATConstants.twoSecondWaitTime);
-  I.submitAllPaymentInformation();
   I.Logout();
 });
