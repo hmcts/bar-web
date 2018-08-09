b49befc9a4141832b73663e44c7be35fe00489c1
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-08-09 15:06:05 +0100

    Latest changes for BAR-313

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './components/card/card.component';
+import { StatsComponent } from './components/stats/stats.component';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [CardComponent],
-  exports: [CardComponent]
+  declarations: [CardComponent, DetailsComponent, StatsComponent],
+  exports: [CardComponent, DetailsComponent, StatsComponent]
 })
 export class SharedModule { }
