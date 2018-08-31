378c07616b35a88daec5ef542d36f64e1ed76f70
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-08-31 18:45:01 +0100

    User acceptance test added.

--- src/app/core/components/payment-instruction-list/payment-instruction-list.component.ts
+++ src/app/core/components/payment-instruction-list/payment-instruction-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { IPaymentsLog } from '../../interfaces/payments-log';
 import { PaymentslogService } from '../../services/paymentslog/paymentslog.service';
 import { PaymentStatus } from '../../models/paymentstatus.model';
