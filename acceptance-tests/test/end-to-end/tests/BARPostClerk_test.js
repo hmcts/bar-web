80b55861da34db4a320d11c904b32c0b4e14508b
Your Name <you@example.com>
2018-09-18 14:11:16 +0100

    Updated logic for editable amount

--- acceptance-tests/test/end-to-end/tests/BARPostClerk_test.js
+++ acceptance-tests/test/end-to-end/tests/BARPostClerk_test.js
@@ -9,7 +9,7 @@ BeforeSuite(I => {
 
 Scenario('Add Payment Instruction', I => {
   I.login('barpreprodpostclerk@mailinator.com', 'LevelAt12');
-  I.waitForText('Add Payment Instruction', BARATConstants.thirtySecondWaitTime);
+  I.retry(BARATConstants.retryCountForStep).waitForText('Add Payment Instruction', BARATConstants.thirtySecondWaitTime);
   I.see('Add Payment Instruction');
   I.see('Payment Type');
   I.waitForElement({ css: '[type="radio"]' }, BARATConstants.thirtySecondWaitTime);
