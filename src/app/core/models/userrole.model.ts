5ae0bd239b200ac0eb63770f942377c3d817ec93
Your Name <you@example.com>
2018-09-24 17:18:01 +0100

    Updated the display name for different roles

--- src/app/core/models/userrole.model.ts
+++ src/app/core/models/userrole.model.ts
@@ -1,6 +1,10 @@
 export class UserRole {
   static FEECLERK = 'bar-fee-clerk';
+  static FEECLERKDISPLAY = 'Fee Clerk';
   static POSTCLERK = 'bar-post-clerk';
+  static POSTCLERKDISPLAY = 'Post Clerk';
   static SRFEECLERK = 'bar-senior-clerk';
+  static SRFEECLERKDISPLAY = 'Reviewer';
   static DELIVERYMANAGER = 'bar-delivery-manager';
+  static DELIVERYMANAGERDISPLAY = 'Approver';
 }
