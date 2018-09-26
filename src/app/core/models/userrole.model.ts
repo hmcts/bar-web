cb14fb8b2de20f93f2d49d4c5955a1eac40d86a6
Your Name <you@example.com>
2018-09-26 22:05:45 +0100

    Updated based on review comments

--- src/app/core/models/userrole.model.ts
+++ src/app/core/models/userrole.model.ts
@@ -1,10 +1,13 @@
+import { first, isEmpty } from 'lodash';
+
+interface IRole {
+  id: string;
+  name: string;
+}
+
 export class UserRole {
-  static FEECLERK = 'bar-fee-clerk';
-  static FEECLERKDISPLAY = 'Fee Clerk';
-  static POSTCLERK = 'bar-post-clerk';
-  static POSTCLERKDISPLAY = 'Post Clerk';
-  static SRFEECLERK = 'bar-senior-clerk';
-  static SRFEECLERKDISPLAY = 'Reviewer';
-  static DELIVERYMANAGER = 'bar-delivery-manager';
-  static DELIVERYMANAGERDISPLAY = 'Approver';
+  static postClerkUser: IRole = { id: 'bar-post-clerk', name: 'Post Clerk' };
+  static feeClerkUser: IRole = { id: 'bar-fee-clerk', name: 'Fee Clerk' };
+  static srFeeClerkUser: IRole = { id: 'bar-senior-clerk', name: 'Reviewer' };
+  static deliveryManagerUser: IRole = { id: 'bar-delivery-manager', name: 'Approver' };
 }
