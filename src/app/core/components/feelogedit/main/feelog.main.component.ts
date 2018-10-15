86405ffa91e897b0c10d61cc9574f10ee834ea99
Your Name <you@example.com>
2018-10-15 11:53:55 +0100

    Added tests

--- src/app/core/components/feelogedit/main/feelog.main.component.ts
+++ src/app/core/components/feelogedit/main/feelog.main.component.ts
@@ -65,7 +65,7 @@ export class FeelogMainComponent implements OnInit {
   getEditTypes() {
     return EditTypes;
   }
-
+/*
   isActionDisabled(action: PaymentAction): boolean {
     if (action === PaymentAction.PROCESS) {
       return this.checkIfRefundExists() || this.model.unallocated_amount !== 0;
@@ -80,7 +80,7 @@ export class FeelogMainComponent implements OnInit {
       );
     }
   }
-
+*/
   submitAction() {
     if (!this.selectedAction) {
       this.showError = true;
