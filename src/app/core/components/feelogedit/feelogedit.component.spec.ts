f2a67aa69f3c1a837bb37d22ec8b1119099bfa41
Attila Kiss <attila.kiss@hmcts.net>
2018-10-17 15:33:44 +0100

    modify merge to be angular 6 compatible

--- src/app/core/components/feelogedit/feelogedit.component.spec.ts
+++ src/app/core/components/feelogedit/feelogedit.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
