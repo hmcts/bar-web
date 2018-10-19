741a4de838ff65179821245e0006db5b3dde13c6
Attila Kiss <attila.kiss@hmcts.net>
2018-10-19 10:49:40 +0100

    [BAR-392][AK] Withdraw

--- src/app/core/components/feelogedit/feelogedit.component.ts
+++ src/app/core/components/feelogedit/feelogedit.component.ts
@@ -275,6 +275,15 @@ export class FeelogeditComponent implements OnInit {
     this.returnModalOn = true;
   }
 
+  onWithdrawPayment() {
+    this.model.action = PaymentAction.WITHDRAW;
+    this.model.status = PaymentStatus.VALIDATED;
+    this.feeLogService.updatePaymentModel(this.model).then(res => {
+      this.toggleReturnModal();
+      return this.router.navigateByUrl('/feelog');
+    });
+  }
+
   onPaymentReversion(e: undefined) {
     const paymentInstructionModel: PaymentInstructionModel = _.clone(
       this.model
