539224b3982fe327c906f2b8a613b03eb6853df0
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-10-31 08:54:25 +0000

    New tests added.

---
+++ src/app/shared/services/action/paymentaction.service.spec.ts
@@ -0,0 +1,30 @@
+import { PaymentActionService } from './paymentaction.service';
+import { BarHttpClient } from '../httpclient/bar.http.client';
+import { mock, instance } from 'ts-mockito';
+import { HttpClient } from '@angular/common/http';
+import { Meta } from '@angular/platform-browser';
+import { of } from 'rxjs';
+
+describe('PaymentActionService', () => {
+  let http, paymentActionService;
+
+  beforeEach(() => {
+    http = new BarHttpClient(instance(mock(HttpClient)), instance(mock(Meta)));
+    paymentActionService = new PaymentActionService(http);
+  });
+
+  fit('should load service', () => {
+    expect(paymentActionService).toBeTruthy();
+  });
+
+  fit('should send the right response', () => {
+    let calledWithParam;
+    spyOn(http, 'get').and.callFake(param => {
+      calledWithParam = param;
+      return of({ data: [], success: true });
+    });
+
+    paymentActionService.getPaymentActions();
+    expect(calledWithParam).toEqual('/api/payment-action');
+  });
+});
