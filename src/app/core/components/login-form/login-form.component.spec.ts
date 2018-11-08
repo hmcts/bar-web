e402f11e6a8635fe3f52b209b91ad8167e42c9c5
Sho CARTER-DANIEL <s.carter-daniel@outlook.com>
2018-11-08 18:32:18 +0000

    New tests added.

--- src/app/core/components/login-form/login-form.component.spec.ts
+++ src/app/core/components/login-form/login-form.component.spec.ts
@@ -4,6 +4,8 @@ import { LoginFormComponent } from './login-form.component';
 import { FormsModule } from '@angular/forms';
 import { UserService } from '../../../shared/services/user/user.service';
 import { CookieService } from 'ngx-cookie-service';
+import { UserModel } from '../../models/user.model';
+import { LoginFormModel } from './login-form.model';
 
 describe('LoginFormComponent', () => {
   let component: LoginFormComponent;
@@ -26,5 +28,43 @@ describe('LoginFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit onAuthenticated() event', () => {
+    const e = { preventDefault() { } };
+    spyOn(e, 'preventDefault');
+    spyOn(component, 'findUser');
+    spyOn(component.onAuthenticated, 'emit');
+
+    component.model = new LoginFormModel(new UserModel({
+      id: 365760,
+      courtId: 'BR04',
+      email: 'barpreprod@mailinator.com',
+      forename: 'PreProd',
+      surname: 'Admin',
+      password: 'password',
+      roles: ['bar-delivery-manager']
+    }));
+
+    component.onSubmit(e);
+
+    expect(e.preventDefault).toHaveBeenCalled();
+    expect(component.findUser).toHaveBeenCalledWith(component.model.email);
+    expect(component.onAuthenticated.emit).toHaveBeenCalled();
+  });
+
+  it('should select the right user', () => {
+    const user = new UserModel({
+      id: 365760,
+      courtId: 'BR04',
+      email: 'barpreprod@mailinator.com',
+      forename: 'PreProd',
+      surname: 'Admin',
+      password: 'password',
+      roles: ['bar-delivery-manager']
+    });
+
+    component.selectUser(user);
+    expect(component.model.email).toEqual(user.email);
   });
 });
