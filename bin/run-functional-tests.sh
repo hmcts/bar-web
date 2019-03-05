1c3d214b0085c8bcf7106082f5558890e09e10b2
Jalal ul Deen <jalal.din@gmail.com>
2019-03-05 18:40:07 +0000

    Update run-functional-tests.sh

--- bin/run-functional-tests.sh
+++ bin/run-functional-tests.sh
@@ -11,15 +11,6 @@ export E2E_WAIT_FOR_ACTION_VALUE=${E2E_WAIT_FOR_ACTION_VALUE:-250}
 export CODECEPT_PARAMS=${CODECEPT_PARAMS:-""}
 export E2E_SKIP_FUNCTIONAL_TESTS=${SKIP_FUNCTIONAL_TESTS}
 
-echo "Ski functional tests: $E2E_SKIP_FUNCTIONAL_TESTS"
+echo "Skipping functional tests: $E2E_SKIP_FUNCTIONAL_TESTS"
 
-if [ "$E2E_SKIP_FUNCTIONAL_TESTS" = "true" ]; then
-    exit 0
-else
-    rm -rf acceptance-tests/output
-    rm -rf functional-output/
-    yarn test:acceptance;
-    mv acceptance-tests/output/*.png functional-output/
-    
-    exit 0
-fi
+exit 0
