cb9fb8c9f52123b4bd8aedbcea0b33ffd2daa869
Elviani Huseyin <elviani_s@hotmail.com>
2019-04-24 13:52:27 +0100

    adding unit test for start date picker

--- src/app/core/components/reporting/reporting.component.spec.ts
+++ src/app/core/components/reporting/reporting.component.spec.ts
@@ -5,6 +5,7 @@ import { PaymentslogService } from '../../services/paymentslog/paymentslog.servi
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BarHttpClient } from '../../../shared/services/httpclient/bar.http.client';
+import * as moment from 'moment';
 
 describe('ReportingComponent', () => {
   let component: ReportingComponent;
@@ -28,6 +29,15 @@ describe('ReportingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should added start date to current date when select date click', async(() => {
+    fixture.debugElement.nativeElement.querySelector('#select-date').click();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      const currentDate = moment().format('DDMMYYYY');
+      expect(component.startDate).toEqual(currentDate);
+    });
+  }));
 
   it('should append "startDate" to the generatedReportingURL.', () => {
     component.startDate = '29042020';
