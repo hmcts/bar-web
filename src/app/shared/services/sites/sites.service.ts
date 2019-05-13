4c2ebad79bc3adce0ce46ad696c1e9363013aac2
Elviani Huseyin <elviani_s@hotmail.com>
2019-05-13 17:21:41 +0100

    adding currentSite

--- src/app/shared/services/sites/sites.service.ts
+++ src/app/shared/services/sites/sites.service.ts
@@ -1,25 +1,45 @@
-import { Injectable, OnDestroy } from '@angular/core';
-import { Observable, Subject, of } from 'rxjs';
+import { Injectable} from '@angular/core';
+import {BehaviorSubject, Observable,  Subject,  of} from 'rxjs';
 import { SitesModel } from '../../../core/models/sites.model';
 import { BarHttpClient } from '../httpclient/bar.http.client';
+import { CookieService } from 'ngx-cookie-service';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class SitesService {
   private sites: Observable<SitesModel[]>;
+  private currentSiteSubject: BehaviorSubject<SitesModel>;
+  private currentSite$: Observable<SitesModel>;
 
-  constructor(private http: BarHttpClient) {}
+  constructor(private http: BarHttpClient, private cookieService: CookieService, private userService: UserService) {
+    this.currentSiteSubject = new BehaviorSubject<SitesModel>(new SitesModel());
+    this.currentSite$ = this.currentSiteSubject.asObservable();
+  }
 
-  // getSites(email: string): Observable<SitesModel[]> {
-  //   if (this.sites) {
-  //     return this.sites;
-  //   }
-  //   this.sites = this.http.get(`/api/sites/users/${email}`);
-  //   return this.sites;
-  // }
+  getSites(): Observable<SitesModel[]> {
+    if (this.sites) {
+      return this.sites;
+    }
+    this.sites = this.http.get(`/api/sites/users/${this.userService.getUser().email}`);
+    return this.sites;
+  }
 
-  getSites(email: string): Observable<SitesModel[]> {
-    return of([{id: 'Y431', description: 'BROMLEY COUNTY COURT', email: []},
-    {id: 'SITE2', description: 'SITE2 COUNTY COURT', email: []},
-    {id: 'SITE3', description: 'SITE3 COUNTY COURT', email: []}]);
+  getCurrentSite(): Observable<SitesModel> {
+    this.getSites().subscribe(result => {
+      let data: SitesModel;
+      if (this.cookieService.get(UserService.SITEID_COOKIE)) {
+        data = result.find(site => site.id === this.cookieService.get(UserService.SITEID_COOKIE));
+      }
+      if (!data) {
+        data = result[0];
+      }
+      this.currentSiteSubject.next(data);
+    });
+    return this.currentSiteSubject;
+  }
+
+  setCurrentSite(site: SitesModel) {
+    this.cookieService.set(UserService.SITEID_COOKIE, site.id);
+    this.currentSiteSubject.next(site);
   }
 }
