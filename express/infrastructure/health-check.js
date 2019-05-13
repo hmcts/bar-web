563ce9c31f4fbc5b60d76259bcac9eae08a396c6
attila-kiss-hmcts <36731933+attila-kiss-hmcts@users.noreply.github.com>
2019-05-13 16:53:06 +0100

    [BAR-685] health check (#435)
    * [BAR-685][AK] implement circuit breaker in nodejs
    
    * [BAR-685][AK] add fee-search as a dependency to health check
    
    * [BAR-685][AK] add circuitbreak to health-check
    
    * [BAR-685][AK] move health check file under folder
    
    * [BAR-685][AK] remove health-check.js from sonar
    
    * [BAR-685][AK] use root url for fee-search

---
+++ express/infrastructure/health-check.js
@@ -0,0 +1,61 @@
+const HttpStatus = require('http-status-codes');
+const request = require('client-request');
+const config = require('config');
+const circuitBreaker = require('opossum');
+
+const feeUrl = config.has('fee.jurisdictionsUrl') ? config.get('fee.jurisdictionsUrl') : '';
+
+function asyncFunctionThatCouldFail() {
+  return new Promise((resolve, reject) => {
+    const status = {};
+    request({
+      uri: `${feeUrl}/health/liveness`,
+      method: 'GET',
+      timeout: 1000,
+      json: true
+    }, (err, response) => {
+      if (err) {
+        reject(err);
+        return;
+      } else if (response.statusCode > HttpStatus.CREATED) {
+        status.status = 'DOWN';
+        status.details = {
+          fee_search: {
+            status: 'DOWN',
+            details: `status_code: ${response.statusCode}`
+          }
+        };
+      } else {
+        status.status = 'UP';
+        status.details = { fee_search: { status: 'UP' } };
+      }
+      resolve(status);
+    });
+  });
+}
+
+const options = {
+  timeout: 3000,
+  errorThresholdPercentage: 50,
+  resetTimeout: 30000
+};
+const breaker = circuitBreaker(asyncFunctionThatCouldFail, options);
+
+
+function check(req, res) {
+  breaker.fire()
+    .then(status => res.status(HttpStatus.OK).json(status))
+    .catch(err => {
+      res.status(HttpStatus.OK).json({
+        status: 'DOWN',
+        details: {
+          fee_search: {
+            status: 'DOWN',
+            details: err
+          }
+        }
+      });
+    });
+}
+
+module.exports = check;
