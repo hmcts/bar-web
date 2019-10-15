5d0acf5bb2c6a80bd2558b098c0813656f90bc68
Santosh Siva Kumar <490436@cognizant.com>
2019-10-15 12:19:23 +0100

    GTAG

--- src/assets/js/gtags.js
+++ src/assets/js/gtags.js
@@ -1,6 +1,6 @@
 window.dataLayer = window.dataLayer || [];
-	function gtag() {
-	  // eslint-disable-next-line prefer-rest-params
-	  dataLayer.push(arguments);
-	}
-	gtag('js', new Date());
+function gtag() {
+  // eslint-disable-next-line prefer-rest-params
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
