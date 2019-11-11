2cff281cfd1f4417557425f34ab72fe3eeed7d4e
Anil Dhawale <anil.dhawale@hmcts.net>
2019-11-11 14:08:11 +0000

    functional test cases fix

--- src/app/shared/services/user/user.service.ts
+++ src/app/shared/services/user/user.service.ts
@@ -7,8 +7,10 @@ export class UserService {
   static USER_COOKIE = '__user-info';
   static SITEID_COOKIE = '__site-id';
   static AUTH_TOKEN = '__auth-token';
+  static USER_SCOPE_COOKIE = '__user_scope';
+
   static USERS = ['post.clerk@hmcts.net', 'fee.clerk@hmcts.net', 'seniorfee.clerk@hmcts.net', 'delivery.manager@hmcts.net',
-    'seniorfee.clerk2@hmcts.net', 'barpreprodpostclerk@mailinator.com', 'barpreprodfeeclerk@mailinator.com',
+    'seniorfee.clerk2@hmcts.net', 'barpreprodpostclerk@mailinator.com', 'barpreprodfeeclerk1@mailinator.com',
     'barpreprodsrfeeclerk@mailinator.com', 'barpreprod@mailinator.com', 'post.clerk.bromley@hmcts.net', 'fee.clerk.bromley@hmcts.net',
     'seniorfee.clerk.bromley@hmcts.net', 'delivery.manager.bromley@hmcts.net', 'post.clerk.milton@hmcts.net', 'fee.clerk.milton@hmcts.net',
     'seniorfee.clerk.milton@hmcts.net', 'delivery.manager.milton@hmcts.net', 'site2feeclerk@mailinator.com', 'SiteSwitchFee@mailnesia.com',
@@ -26,6 +28,7 @@ export class UserService {
 
   authenticate(userModel: UserModel, siteIds: string[]): boolean {
     if (UserService.USERS.includes(userModel.email)) {
+      console.log('authenticate', siteIds);
       this.storeUser(userModel, siteIds);
       return true;
     }
@@ -41,12 +44,13 @@ export class UserService {
     } else {
       siteId = siteIds[0];
     }
+    console.log('user cookie', UserService.SITEID_COOKIE);
     this._cookieService.set(UserService.SITEID_COOKIE, siteId);
     this._cookieService.set(UserService.AUTH_TOKEN, user.email);
   }
 
   logOut(): void {
     this._cookieService.delete(UserService.USER_COOKIE);
-    this._cookieService.delete(UserService.AUTH_TOKEN);
+    this._cookieService.delete(UserService.USER_SCOPE_COOKIE);
   }
 }
