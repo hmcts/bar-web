df8634d67803b49c4248bbdd4d183c908c4fbb5a
santu412 <santu412@gmail.com>
2020-03-24 14:17:28 +0000

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -42,7 +42,8 @@ module "bar-web" {
   asp_rg = "${local.rgName}"
   app_settings = {
     IDAM_API_URL = "${var.idam_api_url}"
-    IDAM_AUTHENTICATION_WEB_URL = "${var.authentication_web_url}/o/authorize"
+    IDAM_LOGIN_URL= "${var.authentication_web_url}/o/authorize"
+    IDAM_AUTHENTICATION_WEB_URL = "${var.authentication_web_url}"
     IDAM_CLIENT_SECRET = "${data.azurerm_key_vault_secret.idam_client_secret.value}"
     BAR_API_URL = "http://bar-api-${local.local_env}.service.${local.local_ase}.internal"
 
