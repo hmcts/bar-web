0eb86b58da0848ff14d058c74848de4736ce4751
Santosh Siva Kumar <490436@ambgb000021.home>
2020-06-25 12:05:40 +0100

    BUILDFIX

--- acceptance-tests/test/end-to-end/tests/BARDeliveryManager_test.js
+++ acceptance-tests/test/end-to-end/tests/BARDeliveryManager_test.js
@@ -31,7 +31,7 @@ Scenario('Assign users to site and turn on features', I => {
     .then(resp => console.log('milton response', resp))
     .then(() => {
       I.amOnPage('/features');
-      I.waitForElement('#send-to-payhub', BARATConstants.fiveSecondWaitTime);
+      I.waitForText('Feature UID', BARATConstants.fiveSecondWaitTime);
       I.turnAllFeatureOn();
       I.click('Save');
     })
