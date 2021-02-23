40a2492b9f3621bce2b8f3488105fe4a471f6c63
vigneshpushkaranHMCTS <vignesh.pushkaran@hmcts.net>
2021-02-23 15:28:36 +0000

    Update paymentsoverview.service.mock.ts

--- src/app/core/test-mocks/paymentsoverview.service.mock.ts
+++ src/app/core/test-mocks/paymentsoverview.service.mock.ts
@@ -4,14 +4,13 @@
 import { Observable } from 'rxjs';
 import { IResponse } from '../interfaces';
 import { PaymentTypeEnum } from '../models/payment.type.enum';
-import {of} from 'rxjs/internal/observable/of';
+import { of } from 'rxjs/internal/observable/of';
 
 const paymentTypeEnum = new PaymentTypeEnum();
 
 export const stats = `
 {
-  "content":{
-     "0":[
+   "0":[
         {
            "count":1,
            "total_amount":55000,
@@ -74,8 +73,7 @@ export const stats = `
               }
            }
         }
-     ]
-  },
+     ],
   "_links":{
      "self":{
         "href":"http://localhost:8080/users/365751/payment-instructions/stats?status=PA"
@@ -85,71 +83,71 @@ export const stats = `
 `;
 export class PaymentsOverviewServiceMock {
 
-  getPaymentsOverview(status: string): Observable<IResponse> {
-    const data = [];
-    const success = true;
+   getPaymentsOverview(status: string): Observable<IResponse> {
+      const data = [];
+      const success = true;
 
-    return new Observable(observer => {
-      observer.next({ data, success });
-      observer.complete();
-    });
-  }
+      return new Observable(observer => {
+         observer.next({ data, success });
+         observer.complete();
+      });
+   }
 
-  getRecordedData(status: string): Observable<IResponse> {
-   const data = [];
-   const success = true;
+   getRecordedData(status: string): Observable<IResponse> {
+      const data = [];
+      const success = true;
 
-   return new Observable(observer => {
-     observer.next({ data, success });
-     observer.complete();
-   });
- }
- 
-  getPaymentStatsByUserAndStatus(userId, status): Observable<IResponse> {
-    const data = JSON.parse(stats);
-    const success = true;
+      return new Observable(observer => {
+         observer.next({ data, success });
+         observer.complete();
+      });
+   }
 
-    return new Observable(observer => {
-      observer.next({ data, success });
-      observer.complete();
-    });
-  }
+   getPaymentStatsByUserAndStatus(userId, status): Observable<IResponse> {
+      const data = JSON.parse(stats);
+      const success = true;
 
-  getPaymentInstructionCount(status: string): Observable<IResponse> {
-    return of({success: true, data: 0});
-  }
+      return new Observable(observer => {
+         observer.next({ data, success });
+         observer.complete();
+      });
+   }
+
+   getPaymentInstructionCount(status: string): Observable<IResponse> {
+      return of({ success: true, data: 0 });
+   }
 
 
 
-  getRejectedPaymentsOverview(oldStatus: string, newStatus: string): Observable<IResponse> {
-    return of({
-      data: {
-        '365751': [
-          {
-            'bar_user_id': '365751',
-            'bar_user_full_name': 'Karen Taylor',
-            'count_of_payment_instruction_in_specified_status': 1,
-            'list_of_payment_instructions': [1, 2, 3]
-          }
-        ],
-        '365752': [
-          {
-            'bar_user_id': '365752',
-            'bar_user_full_name': 'James Black',
-            'count_of_payment_instruction_in_specified_status': 2,
-            'list_of_payment_instructions': [1, 2, 3]
-          }
-        ],
-        '365756': [
-          {
-            'bar_user_id': '365756',
-            'bar_user_full_name': 'James2 Black2',
-            'count_of_payment_instruction_in_specified_status': 1,
-            'list_of_payment_instructions': [1, 2, 3]
-          }
-        ]
-      },
-      success: true
-    });
-  }
+   getRejectedPaymentsOverview(oldStatus: string, newStatus: string): Observable<IResponse> {
+      return of({
+         data: {
+            '365751': [
+               {
+                  'bar_user_id': '365751',
+                  'bar_user_full_name': 'Karen Taylor',
+                  'count_of_payment_instruction_in_specified_status': 1,
+                  'list_of_payment_instructions': [1, 2, 3]
+               }
+            ],
+            '365752': [
+               {
+                  'bar_user_id': '365752',
+                  'bar_user_full_name': 'James Black',
+                  'count_of_payment_instruction_in_specified_status': 2,
+                  'list_of_payment_instructions': [1, 2, 3]
+               }
+            ],
+            '365756': [
+               {
+                  'bar_user_id': '365756',
+                  'bar_user_full_name': 'James2 Black2',
+                  'count_of_payment_instruction_in_specified_status': 1,
+                  'list_of_payment_instructions': [1, 2, 3]
+               }
+            ]
+         },
+         success: true
+      });
+   }
 }
