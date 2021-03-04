3498fd728b38f6b8cd96a5bb0f84f904be5373db
Satish Ramachandrappa <satish.ramachandrappa@hmcts.net>
2021-03-04 18:07:36 +0000

    fixing minor typo  issue

--- acceptance-tests/test/end-to-end/tests/BARDeliveryManager_test.js
+++ acceptance-tests/test/end-to-end/tests/BARDeliveryManager_test.js
@@ -153,7 +153,7 @@ Scenario('User admin console', I => {
 
 Scenario('Confirm transfer to BAR', I => {
   I.login('barpreprod@mailinator.com', 'LevelAt12');
-  I.waitForText('Payments overview', BARATConstants.tenSecondWaitTime);q
+  I.waitForText('Payments overview', BARATConstants.tenSecondWaitTime);
   if (testSendToPayhub) {
     I.amOnPage('/features');
     I.wait(BARATConstants.twelveSecondWaitTime);
