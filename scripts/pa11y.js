9ff9a84c5f39cf81a1240dec8b7bd4c812178de3
shravan mechineni <shravanmechineni5@gmail.com>
2022-08-08 16:35:46 +0100

    update pa11y options config and reporting

--- scripts/pa11y.js
+++ scripts/pa11y.js
@@ -4,11 +4,12 @@ const pa11y = require('pa11y');
 const fs = require('fs');
 const htmlReporter = require('pa11y-reporter-html');
 const config = require('../acceptance-tests/test/end-to-end/config/BARConfig');
+const outputFolder = 'functional-output';
 const url = process.env.TEST_URL;
 
 // Generates HTML reporter
 const generateHTMLReport = html => new Promise((resolve, reject) => {
-  fs.appendFile('functional-output/a11y.html', html, err => {
+  fs.appendFile(`${outputFolder}/a11y.html`, html, err => {
     if (err) reject(new Error(err));
     resolve({ message: 'a11y report file updated.' });
   });
@@ -18,18 +19,6 @@ async function runTest() {
 
   try {
 
-    // Put together some options to use in each test
-    const options = {
-      wait: 1000,
-      timeout: 70000,
-      chromeLaunchConfig: { slowMo: 500 },
-      log: {
-        debug: console.log,
-        error: console.error,
-        info: console.log
-      }
-    };
-
     // Pages running the tests
     console.log("payment-overview page");
     const pa11yResult1 = await pa11y(`${url}/payment-overview`, {
@@ -38,10 +27,16 @@ async function runTest() {
         `set field #username to ${config.TestBarDeliveryManagerUserName}`,
         `set field #password to ${config.TestBarDeliveryManagerPassword}`,
         'click element .button',
-        'wait for element .heading-xlarge to be visible',
-        'screen capture functional-output/payment-overview.png',
+        'wait for element .heading-xlarge to be visible'
       ],
-      options,
+      wait: 1000,
+      timeout: 70000,
+      screenCapture: `${outputFolder}/payment-overview.png`,
+      log: {
+        debug: console.log,
+        error: console.error,
+        info: console.log
+      }
     });
 
     console.log("reporting page");
@@ -52,10 +47,16 @@ async function runTest() {
         `set field #password to ${config.TestBarDeliveryManagerPassword}`,
         'click element .button',
         'wait for element .heading-xlarge to be visible',
-        'screen capture functional-output/reporting.png',
         'click element #advanced-search-link'
       ],
-      options
+      wait: 1000,
+      timeout: 70000,
+      screenCapture: `${outputFolder}/reporting.png`,
+      log: {
+        debug: console.log,
+        error: console.error,
+        info: console.log
+      }
     });
 
     console.log("user-admin page");
@@ -66,9 +67,15 @@ async function runTest() {
         `set field #password to ${config.TestBarDeliveryManagerPassword}`,
         'click element .button',
         'wait for element .heading-xlarge to be visible',
-        'screen capture functional-output/user-admin.png',
       ],
-      options
+      wait: 1000,
+      timeout: 70000,
+      screenCapture: `${outputFolder}/user-admin.png`,
+      log: {
+        debug: console.log,
+        error: console.error,
+        info: console.log
+      }
     });
 
     console.log("dashboard and add payment info page");
@@ -79,16 +86,23 @@ async function runTest() {
         `set field #password to ${config.TestBarFeeClerkPassword}`,
         'click element .button',
         'wait for element .heading-xlarge to be visible',
-        'screen capture functional-output/dashboard-payment.png',
         'wait for element #payment_type_CARD to be visible',
         'click element #payment_type_CARD',
+        'wait for element #payer-name to be visible',
         'set field #payer-name to Test Payer',
         'set field #amount to 100',
         'wait for element #authorization-code to be visible',
         'set field #authorization-code to 123456',
         'click element .button',
       ],
-      options
+      wait: 1000,
+      timeout: 70000,
+      screenCapture: `${outputFolder}/dashboard-payment.png`,
+      log: {
+        debug: console.log,
+        error: console.error,
+        info: console.log
+      }
     });
 
     console.log("paymentslog page");
@@ -99,9 +113,15 @@ async function runTest() {
         `set field #password to ${config.TestBarFeeClerkPassword}`,
         'click element .button',
         'wait for element .heading-xlarge to be visible',
-        'screen capture functional-output/paymentslog.png',
       ],
-      options
+      wait: 1000,
+      timeout: 70000,
+      screenCapture: `${outputFolder}/paymentslog.png`,
+      log: {
+        debug: console.log,
+        error: console.error,
+        info: console.log
+      }
     });
 
     console.log("feelog page");
@@ -112,9 +132,15 @@ async function runTest() {
         `set field #password to ${config.TestBarSwitchSitePassword}`,
         'click element .button',
         'wait for element .heading-xlarge to be visible',
-        'screen capture functional-output/feelog.png',
       ],
-      options
+      wait: 1000,
+      timeout: 70000,
+      screenCapture: `${outputFolder}/feelog.png`,
+      log: {
+        debug: console.log,
+        error: console.error,
+        info: console.log
+      }
     });
 
     const pa11yResults = [pa11yResult1, pa11yResult2, pa11yResult3, pa11yResult4, pa11yResult5, pa11yResult6];
