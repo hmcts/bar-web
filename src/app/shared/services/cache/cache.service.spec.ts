6f37c597195a7a37169c0d61cc0825c9c9b2f278
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2023-10-10 17:08:53 +0100

    new test class

---
+++ src/app/shared/services/cache/cache.service.spec.ts
@@ -0,0 +1,32 @@
+import { CacheService } from "./cache.service";
+import { instance, mock } from 'ts-mockito/lib/ts-mockito';
+import { HttpClient } from '@angular/common/http';
+import { Meta } from '@angular/platform-browser';
+import { of } from 'rxjs';
+
+describe('CacheService', () => {
+  
+    let http, cacheService;
+
+    beforeEach(() => {
+        http = instance(mock(HttpClient)), instance(mock(Meta));
+        cacheService = new CacheService();
+      });
+
+  fit('notify all observers', done => {  
+    let calledWithParam;
+    spyOn(http, 'get').and.callFake(param => {
+      calledWithParam = param;
+      return of({ data: [], success: true });
+    });
+
+    cacheService.set('foo', 'bar', 10000);
+    cacheService.get('features', http.get(`/api/features`))
+    .subscribe(res => {
+        expect(cacheService.has('foo').toBeTruthy);
+        done();
+      });
+  });
+
+
+});
