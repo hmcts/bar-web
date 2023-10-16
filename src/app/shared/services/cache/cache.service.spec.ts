ebd65859f85575dc1c13c66f1e4d07c01c826612
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2023-10-16 16:16:15 +0100

    test notifies in flight observers

--- src/app/shared/services/cache/cache.service.spec.ts
+++ src/app/shared/services/cache/cache.service.spec.ts
@@ -1,30 +1,30 @@
 import { CacheService } from "./cache.service";
 import { instance, mock } from 'ts-mockito/lib/ts-mockito';
 import { HttpClient } from '@angular/common/http';
-import { Meta } from '@angular/platform-browser';
-import { of } from 'rxjs';
-import {async, TestBed} from "@angular/core/testing";
+import { Subject, of } from 'rxjs';
+import { async, TestBed } from "@angular/core/testing";
 
 
 describe('CacheService', () => {
-  
-    let http, cacheServiceToBeTested;
 
-    beforeEach(() => {
-        http = instance(mock(HttpClient)), instance(mock(Meta));
-        cacheServiceToBeTested = new CacheService();
-      });
+  let http, cacheServiceToBeTested;
+  let subject;
+
+  beforeEach(() => {
+    cacheServiceToBeTested = new CacheService();
+    spyOn(console, 'log').and.callThrough();
+  });
 
   it('Test set', async(() => {
     cacheServiceToBeTested = new CacheService();
     cacheServiceToBeTested.set('foo', 'bar', 10000);
     let result = cacheServiceToBeTested.get('foo');
     result.subscribe((data) => {
-        expect(data).toEqual('bar');
-        console.info('Happy days');
-      },
+      expect(data).toEqual('bar');
+      console.info('Happy days');
+    },
       (error) => {
-        console.error('ERROR WA'+ error.toString());
+        console.error('ERROR WA' + error.toString());
       },
       () => {
         console.info('happy days');
@@ -37,6 +37,29 @@ describe('CacheService', () => {
     cacheServiceToBeTested.set('foo', 'bar', 10000);
     let result = cacheServiceToBeTested.has('foo');
     expect(result).toEqual(true);
+  });
+
+  fit('notifies in flight observers', () => {
+
+    // simulate 2 api calls by creating a new Subject and passing it to the cache service twice.
+    cacheServiceToBeTested = new CacheService();
+    const subject1 = new Subject<string>();
+    subject1.subscribe({
+      next: (v) => {
+        //console.log(`observer: ${v}`),
+      }
+    });
+    cacheServiceToBeTested.get("subject1", subject1).subscribe(
+      value => {
+        //console.log('got a value from my subscription:', value)
+      });
+    cacheServiceToBeTested.get("subject1", subject1).subscribe(
+      value => {
+        //console.log('got a value from my subscription:', value)
+      });
+    // The subject needs data to send. This simulates the api coming back with data
+    subject1.next('hello');
+    expect(console.log).toHaveBeenCalled();
   });
 
 });
